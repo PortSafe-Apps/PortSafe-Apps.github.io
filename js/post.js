87470d00164ba5a25633d9d5134e69da89a3a399
fahiraaa06 <fahira1678@gmail.com>
2023-11-09 10:38:57 +0700

    update function login3

--- js/post.js
+++ js/post.js
@@ -1,7 +1,7 @@
 // import { postWithToken } from "https://jscroot.github.io/api/croot.js";
 // import {setInner,getValue} from "https://jscroot.github.io/element/croot.js";
 
-import PostSignUp from "./postfunc.js";
+import PostSignUp from "js/postfunc.js";
 
 
 window.PostSignUp = PostSignUp;
