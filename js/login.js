9637a783b7b0a0cace22c0b7ae783e456e3e34d8
Fahira <80674110+fahiraaa06@users.noreply.github.com>
2023-11-17 13:54:00 +0700

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,3 +1,27 @@
+import { setCookieWithExpireHour } from "https://jscroot.github.io/cookie/croot.js";
+
+// Mengambil nilai dari elemen input pada html
+const loginForm = document.getElementById("loginForm");
+const usernameInput = document.getElementById("username");
+const passwordInput = document.getElementById("psw-input");
+const submitButton = document.getElementById("submit");
+const errorMessage = document.getElementById("error-message");
+
+// Fungsi untuk mengecek apakah form telah diisi dengan benar
+const validation = () => {
+  const username = usernameInput.value;
+  const pass = passwordInput.value;
+  if (username !== "" && pass !== "") {
+    submitButton.disabled = false;
+  } else {
+    submitButton.disabled = true;
+  }
+};
+
+// Panggil fungsi validation saat input berubah
+usernameInput.addEventListener("input", validation);
+passwordInput.addEventListener("input", validation);
+
 loginForm.addEventListener("submit", async (event) => {
   event.preventDefault();
   const username = usernameInput.value;
