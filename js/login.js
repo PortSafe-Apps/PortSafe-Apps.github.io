157e4e5c51444a7f644f89489c4d7a166690d5f5
fahiraaa06 <fahira1678@gmail.com>
2023-12-01 21:09:28 +0700

    update login.js

--- js/login.js
+++ js/login.js
@@ -1,32 +1,13 @@
-import { postWithToken } from 'https://jscroot.github.io/api/croot.js';
-import { PostLogin, redirectBasedOnRole, ResponseLogin } from './config/config.js';
-import { URLLogin, token } from './template/template.js';
-import { decodeToken } from './utils.js';
+import { postWithToken } from "https://jscroot.github.io/api/croot.js";
+import { PostLogin, ResponseLogin } from "./config/config.js";
+import { URLLogin } from "./template/template.js";
+import { token } from './template/template.js';
 
-document.addEventListener('DOMContentLoaded', function() {
-  const form = document.getElementById('loginForm');
-  form.addEventListener('submit', async function(event) {
+document.addEventListener("DOMContentLoaded", function() {
+  const form = document.getElementById("loginForm");
+  form.addEventListener("submit", function(event) {
     event.preventDefault();
     let data = PostLogin();
-
-    try {
-      // Pastikan token tersedia sebelum mengirimkan permintaan
-      console.log('Token:', token);
-
-      const response = await postWithToken(URLLogin, 'Authorization', 'Bearer ' + token, data, ResponseLogin);
-
-      if (response && response.token) {
-        const decodedToken = decodeToken(response.token);
-        
-        // Lakukan redirect berdasarkan peran (role)
-        redirectBasedOnRole(decodedToken);
-      } else {
-        console.error('Token tidak ditemukan dalam respons.');
-        // Handle token not found error, show message, etc.
-      }
-    } catch (error) {
-      console.error('Error:', error);
-      // Handle other errors, show message, etc.
-    }
+    postWithToken(URLLogin, 'Authorization', 'Bearer ' + token, data, ResponseLogin);
   });
 });
