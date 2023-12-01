d90d6a685c8075b2e583558cbf53c137450d7981
fahiraaa06 <fahira1678@gmail.com>
2023-12-01 22:52:32 +0700

    fixing config.js

--- js/config/config.js
+++ js/config/config.js
@@ -1,86 +1,83 @@
 import { setCookieWithExpireHour } from 'https://jscroot.github.io/cookie/croot.js';
-import { decodeToken } from '../utils.js';
 
 //token
 export function getTokenFromAPI() {
-  const tokenUrl = "https://asia-southeast2-ordinal-stone-389604.cloudfunctions.net/login";
-  fetch(tokenUrl)
-    .then(response => response.json())
-    .then(tokenData => {
-      if (tokenData.token) {
-        userToken = tokenData.token;
-        console.log('Token dari API:', userToken);
-      }
-    })
-    .catch(error => console.error('Gagal mengambil token:', error));
-}
+    const tokenUrl = "https://asia-southeast2-ordinal-stone-389604.cloudfunctions.net/login-1";
+    fetch(tokenUrl)
+      .then(response => response.json())
+      .then(tokenData => {
+        if (tokenData.token) {
+          userToken = tokenData.token;
+          console.log('Token dari API:', userToken);
+        }
+      })
+      .catch(error => console.error('Gagal mengambil token:', error));
+  }
+  export function GetDataForm(){
+              const nipp = document.querySelector("#nipp").value;
+              const nama = document.querySelector("#nama").value;
+              const jabatan = document.querySelector("#jabatan").value;
+              const divisi = document.querySelector("#divisi").value;
+              const bidang = document.querySelector("#bidang").value;
+              const password = document.querySelector("#psw-input").value;
+          
+              // Set nilai default role langsung di dalam fungsi
+              const role = "user";
+          
+              const data = {
+                  nipp: nipp,
+                  nama: nama,
+                  jabatan: jabatan,
+                  divisi: divisi,
+                  bidang: bidang,
+                  password: password,
+                  role: role,
+              };
+              return data
+              
+  }
 
-//register
-export function GetDataForm(){
-    const nipp = document.querySelector("#nipp").value;
-    const nama = document.querySelector("#nama").value;
-    const jabatan = document.querySelector("#jabatan").value;
-    const divisi = document.querySelector("#divisi").value;
-    const bidang = document.querySelector("#bidang").value;
-    const password = document.querySelector("#psw-input").value;
-
-    // Set nilai default role langsung di dalam fungsi
-    const role = "user";
-
+  //login
+  export function PostLogin() {
+    const nipp = document.getElementById("nipp").value;
+    const password = document.getElementById("psw-input").value;   
+  
     const data = {
-        nipp: nipp,
-        nama: nama,
-        jabatan: jabatan,
-        divisi: divisi,
-        bidang: bidang,
-        password: password,
-        role: role,
+      nipp: nipp,
+      password: password,
     };
-
     return data;
-}
-
-export function AlertPost(value){
-    alert(value.message + "\nRegistrasi Berhasil")
-    window.location.href= "https://portsafe-apps.github.io/index.html"
-}
-
-//login
-export function PostLogin() {
-  const nipp = document.getElementById("nipp").value;
-  const password = document.getElementById("psw-input").value;
-
-  const data = {
-    nipp: nipp,
-    password: password,
-  };
-  return data;
-}
-
-export function ResponsePostLogin(response) {
+  }
+  
+  
+  function ResponsePostLogin(response) {
     if (response && response.token) {
-    setCookieWithExpireHour('Login', response.token, 2);
-      
-      // Menggunakan fungsi decodeToken dari utils.js
-      const decodedToken = decodeToken(response.token);
-      const userRole = decodedToken.role;
-    
+      setCookieWithExpireHour('Login', response.token, 2);
+  
+      const userRole = response.Role; // Mengambil nilai peran dari respons
+  
       if (userRole === 'user') {
-        window.location.href = 'https://portsafe-apps.github.io/pages/user/beranda.html';
+        window.location.href = 'https://portsafe-apps.github.io/pages/user/beranda.html'; // Redirect untuk role user
       } else if (userRole === 'admin') {
-        window.location.href = 'https://portsafe-apps.github.io/pages/admin/dashboard.html';
+        window.location.href = 'https://portsafe-apps.github.io/pages/admin/dashboard.html'; // Redirect untuk role admin
       } else {
         console.error('Role tidak dikenali:', userRole);
-        // Tangani peran yang tidak dikenali, jika diperlukan
+        // Handle jika role tidak dikenali
       }
     } else {
-      handleLoginError(response.Message);
+      // alert('Login gagal. Silakan coba lagi.');
     }
   }
   
-export function ResponsePost(result) {
-    AlertPost(result);
+  export function AlertPost(value){
+    alert(value.message + "\nRegistrasi Berhasil")
+    window.location.href= "https://portsafe-apps.github.io/"
 }
-export function ResponseLogin(result) {
-  ResponsePostLogin(result)
-}
+
+  export function ResponsePost(result) {
+      AlertPost(result);
+  }
+
+  export function ResponseLogin(result) {
+    ResponsePostLogin(result)
+  }
--- js/template/template.js
+++ js/template/template.js
@@ -1,3 +1,3 @@
 export let URLPost = `https://asia-southeast2-ordinal-stone-389604.cloudfunctions.net/register`
-export let URLLogin = `https://asia-southeast2-ordinal-stone-389604.cloudfunctions.net/login`
+export let URLLogin = `https://asia-southeast2-ordinal-stone-389604.cloudfunctions.net/login-1`
 export let token = 'token';
