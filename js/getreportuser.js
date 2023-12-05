52dc76c1bed3e491acc67ed9505abd238235d644
fahiraaa06 <fahira1678@gmail.com>
2023-12-05 15:28:55 +0700

    create list report

--- js/getreportuser.js
+++ js/getreportuser.js
@@ -1,61 +1,3 @@
-// main.js
-
-// Function to create a card element for each report
-function createReportCard(report) {
-    const card = document.createElement('div');
-    card.className = 'card timeline-card bg-dark';
-
-    const cardBody = document.createElement('div');
-    cardBody.className = 'card-body';
-
-    const headingDiv = document.createElement('div');
-    headingDiv.className = 'd-flex justify-content-between';
-
-    const timelineTextDiv = document.createElement('div');
-    timelineTextDiv.className = 'timeline-text mb-2';
-
-    const reportIdHeading = document.createElement('h6');
-    reportIdHeading.className = 'element-heading fw-bolder';
-    reportIdHeading.textContent = report.Reportid;
-
-    const locationSpan = document.createElement('span');
-    locationSpan.textContent = report.Location.LocationName;
-
-    // Continue creating the rest of the HTML structure...
-
-    // Append elements to their respective parent elements
-    timelineTextDiv.appendChild(reportIdHeading);
-    timelineTextDiv.appendChild(locationSpan);
-
-    // Continue appending other elements...
-
-    headingDiv.appendChild(timelineTextDiv);
-
-    // Continue appending other elements...
-
-    cardBody.appendChild(headingDiv);
-
-    card.appendChild(cardBody);
-
-    return card;
-}
-
-// Function to display report data in the specified container
-function displayReports(reportsData, containerId) {
-    const reportsContainer = document.getElementById(containerId);
-
-    if (reportsData && reportsData.length > 0) {
-        reportsData.forEach(report => {
-            const reportCard = createReportCard(report);
-            reportsContainer.appendChild(reportCard);
-        });
-    } else {
-        // Handle the case where no report data is found
-        reportsContainer.innerHTML = '<p>No report data found</p>';
-    }
-}
-
-
 // Function to make the API request with the token
 async function getUserWithTokenAndDisplay() {
     const token = getTokenFromCookies('Login');
@@ -100,6 +42,97 @@ function getTokenFromCookies(cookieName) {
         }
     }
     return null;
+}
+
+
+function createReportCard(report) {
+    const card = document.createElement('div');
+    card.className = 'card timeline-card bg-dark';
+
+    const cardBody = document.createElement('div');
+    cardBody.className = 'card-body';
+
+    const headingDiv = document.createElement('div');
+    headingDiv.className = 'd-flex justify-content-between';
+
+    const timelineTextDiv = document.createElement('div');
+    timelineTextDiv.className = 'timeline-text mb-2';
+
+    // Report ID and Location
+    const reportIdHeading = document.createElement('h6');
+    reportIdHeading.className = 'element-heading fw-bolder';
+    reportIdHeading.textContent = report.Reportid;
+
+    const locationSpan = document.createElement('span');
+    locationSpan.textContent = report.Location.LocationName;
+
+    // Date badge
+    const dateBadge = document.createElement('span');
+    dateBadge.className = 'badge mb-2 rounded-pill bg-dark';
+    dateBadge.textContent = report.Date;
+
+    // TypeDangerousActions badges
+    const typeDangerousActionsDiv = document.createElement('div');
+    typeDangerousActionsDiv.className = 'timeline-tags';
+
+    report.TypeDangerousActions.forEach(type => {
+        type.SubTypes.forEach(subtype => {
+            const subtypeBadge = document.createElement('span');
+            subtypeBadge.className = 'badge bg-light text-dark';
+            subtypeBadge.textContent = `#${subtype}`;
+            typeDangerousActionsDiv.appendChild(subtypeBadge);
+        });
+    });
+
+    // Append elements to their respective parent elements
+    timelineTextDiv.appendChild(reportIdHeading);
+    timelineTextDiv.appendChild(locationSpan);
+    timelineTextDiv.appendChild(dateBadge);
+    timelineTextDiv.appendChild(typeDangerousActionsDiv);
+
+    headingDiv.appendChild(timelineTextDiv);
+
+    // User information
+    const userDiv = document.createElement('div');
+    userDiv.className = 'text-content mb-2';
+    const userHeading = document.createElement('h6');
+    userHeading.className = 'mb-0';
+    userHeading.textContent = 'User Information';
+
+    const userData = document.createElement('p');
+    const userName = document.createElement('span');
+    userName.textContent = report.User.nama;
+    const userRole = document.createElement('span');
+    userRole.textContent = report.User.jabatan;
+    userData.appendChild(userName);
+    userData.appendChild(document.createElement('br'));
+    userData.appendChild(userRole);
+
+    userDiv.appendChild(userHeading);
+    userDiv.appendChild(userData);
+
+    // Continue creating the rest of the HTML structure...
+
+    cardBody.appendChild(headingDiv);
+    cardBody.appendChild(userDiv);
+
+    card.appendChild(cardBody);
+
+    return card;
+}
+
+function displayReports(reportsData, containerId) {
+    const reportsContainer = document.getElementById(containerId);
+
+    if (reportsData && reportsData.length > 0) {
+        reportsData.forEach(report => {
+            const reportCard = createReportCard(report);
+            reportsContainer.appendChild(reportCard);
+        });
+    } else {
+        // Handle the case where no report data is found
+        reportsContainer.innerHTML = '<p>No report data found</p>';
+    }
 }
 
 // Call the API and display data
