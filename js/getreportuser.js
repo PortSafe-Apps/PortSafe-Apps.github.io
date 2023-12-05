aeb914157860b903b21ecdb0952349a1d0cbbb3b
fahiraaa06 <fahira1678@gmail.com>
2023-12-05 20:51:34 +0700

    perbarui get report 5

--- js/getreportuser.js
+++ js/getreportuser.js
@@ -1,133 +1,99 @@
-// Function to make the API request with the token
-async function getUserWithTokenAndDisplay() {
+async function getUserReportWithToken() {
     const token = getTokenFromCookies('Login');
-
+  
     if (!token) {
-        alert("Token not found");
-        return;
+      Swal.fire({
+        icon: 'warning',
+        title: 'Authentication Error',
+        text: 'Kamu belum login.',
+      });
+      return;
     }
-
+  
     const targetURL = 'https://asia-southeast2-ordinal-stone-389604.cloudfunctions.net/GetAllReportbyUser';
-
+  
     const myHeaders = new Headers();
     myHeaders.append('Login', token);
-
+  
     const requestOptions = {
-        method: 'POST',
-        headers: myHeaders,
-        redirect: 'follow'
+      method: 'GET',
+      headers: myHeaders,
+      redirect: 'follow',
     };
-
+  
     try {
-        const response = await fetch(targetURL, requestOptions);
-        const data = await response.json();
-
-        if (data.status === true) {
-            displayReports(data.data);
-        } else {
-            alert(data.message);
-        }
+      const response = await fetch(targetURL, requestOptions);
+      const data = await response.json();
+  
+      if (data.status === true) {
+        displayReportData(data.data);
+      } else {
+        Swal.fire({
+          icon: 'error',
+          title: 'Error',
+          text: data.message,
+        });
+      }
     } catch (error) {
-        console.error('Error:', error);
+      console.error('Error:', error);
     }
-}
-
-// Function to extract the token from cookies
-function getTokenFromCookies(cookieName) {
+  }
+  
+  function getTokenFromCookies(cookieName) {
     const cookies = document.cookie.split(';');
     for (const cookie of cookies) {
-        const [name, value] = cookie.trim().split('=');
-        if (name === cookieName) {
-            return value;
-        }
+      const [name, value] = cookie.trim().split('=');
+      if (name === cookieName) {
+        return value;
+      }
     }
     return null;
-}
-
-// Function to create a report card
-function createReportCard(report) {
-    const card = document.createElement('div');
-    card.className = 'card timeline-card bg-dark';
-
-    const cardBody = document.createElement('div');
-    cardBody.className = 'card-body';
-
-    const headingDiv = document.createElement('div');
-    headingDiv.className = 'd-flex justify-content-between';
-
-    const timelineTextDiv = document.createElement('div');
-    timelineTextDiv.className = 'timeline-text mb-2';
-
-    // Report ID and Location
-    const reportIdHeading = document.createElement('h6');
-    reportIdHeading.className = 'element-heading fw-bolder';
-    reportIdHeading.textContent = report.Reportid;
-
-    const locationSpan = document.createElement('span');
-    locationSpan.textContent = report.Location.LocationName;
-
-    // Date badge
-    const dateBadge = document.createElement('span');
-    dateBadge.className = 'badge mb-2 rounded-pill bg-dark';
-    dateBadge.textContent = report.Date;
-
-    // TypeDangerousActions badges
-    const typeDangerousActionsDiv = document.createElement('div');
-    typeDangerousActionsDiv.className = 'timeline-tags';
-
-    report.TypeDangerousActions.forEach(type => {
-        type.SubTypes.forEach(subtype => {
-            const subtypeBadge = document.createElement('span');
-            subtypeBadge.className = 'badge bg-light text-dark';
-            subtypeBadge.textContent = `#${subtype}`;
-            typeDangerousActionsDiv.appendChild(subtypeBadge);
-        });
-    });
-
-    // Append elements to their respective parent elements
-    timelineTextDiv.appendChild(reportIdHeading);
-    timelineTextDiv.appendChild(locationSpan);
-    timelineTextDiv.appendChild(dateBadge);
-    timelineTextDiv.appendChild(typeDangerousActionsDiv);
-
-    headingDiv.appendChild(timelineTextDiv);
-
-    // User information
-    const userDiv = document.createElement('div');
-    userDiv.className = 'text-content mb-2';
-    const userHeading = document.createElement('h6');
-    userHeading.className = 'mb-0';
-    userHeading.textContent = 'Pengawas';
-
-    const userData = document.createElement('p');
-    userData.textContent = `${report.User.nama}, ${report.User.jabatan}`;
-
-    userDiv.appendChild(userHeading);
-    userDiv.appendChild(userData);
-
-    cardBody.appendChild(headingDiv);
-    cardBody.appendChild(userDiv);
-
-    card.appendChild(cardBody);
-
-    return card;
-}
-
-// Function to display report data in the specified container
-function displayReports(reportsData) {
-    const reportsContainer = document.getElementById('reportsContainer');
-
-    if (reportsData && reportsData.length > 0) {
-        reportsContainer.innerHTML = ''; // Clear existing content
-        reportsData.forEach(report => {
-            const reportCard = createReportCard(report);
-            reportsContainer.appendChild(reportCard);
-        });
+  }
+  
+  function displayReportData(reportData) {
+    const reportContainer = document.getElementById('reportContainer');
+  
+    // Menghapus semua elemen anak di dalam reportContainer
+    reportContainer.innerHTML = '';
+  
+    if (reportData && reportData.length > 0) {
+      reportData.forEach((report) => {
+        const newCard = document.createElement('div');
+        newCard.className = 'card timeline-card bg-dark';
+        newCard.innerHTML = `
+          <div class="card-body">
+            <div class="d-flex justify-content-between">
+              <div class="timeline-text mb-2">
+                <h6 class="element-heading fw-bolder">${report.reportid}</h6>
+                <span>${report.location.locationName}</span>
+              </div>
+              <div class="timeline-text mb-2">
+                <span class="badge mb-2 rounded-pill bg-dark">${report.date}</span>
+              </div>
+            </div>
+            <div class="divider mt-0"></div>
+            <div class="text-content mb-2">
+              <h6 class="mb-0">Jenis Ketidaksesuaian</h6>
+              <div class="timeline-tags">
+                ${report.typeDangerousActions.map(action => `<span class="badge bg-light text-dark">#${action.typeName}</span>`).join('')}
+              </div>
+            </div>
+            <div class="text-content mb-2">
+              <h6 class="mb-0">Identitas Pengawas</h6>
+              <p><span>${report.user.nama}</span> <br> <span>${report.user.jabatan}</span></p>
+            </div>
+            <!-- Tambahan informasi lainnya sesuai kebutuhan -->
+          </div>
+        `;
+  
+        // Menambahkan card ke dalam reportContainer
+        reportContainer.appendChild(newCard);
+      });
     } else {
-        // Handle the case where no report data is found
-        reportsContainer.innerHTML = '<p>No report data found</p>';
+      reportContainer.innerHTML = '<p>No report data found.</p>';
     }
-}
-
-// Call the API and display data
-getUserWithTokenAndDisplay();
+  }
+  
+  // Panggil fungsi untuk mendapatkan dan menampilkan data report
+  getUserReportWithToken();
+  
