c9ad0d04cf1c574357f3fe68c77a2532b1d8b866
fahiraaa06 <fahira1678@gmail.com>
2023-12-06 07:04:31 +0700

    tampil data report

--- js/getreportuser.js
+++ js/getreportuser.js
@@ -1,42 +1,5 @@
- // Fungsi untuk mendapatkan laporan pengguna dengan token
-async function getUserReportWithToken() {
-    const token = getTokenFromCookies('Login');
-
-    if (!token) {
-        alert("Token tidak ditemukan");
-        return;
-    }
-
-    const targetURL = 'https://asia-southeast2-ordinal-stone-389604.cloudfunctions.net/GetAllReportbyUser';
-
-    const myHeaders = new Headers();
-    myHeaders.append('Login', token);
-
-    const requestOptions = {
-        method: 'POST',
-        headers: myHeaders,
-        redirect: 'follow',
-    };
-
-    try {
-        const response = await fetch(targetURL, requestOptions);
-        const data = await response.json();
-
-        console.log('Server response:', data);
-
-        if (data.status === true) {
-            displayReportData(data.data);
-        } else {
-            console.error('Server response:', data.message || 'Data tidak dapat ditemukan');
-        }
-    } catch (error) {
-        console.error('Error in getUserReportWithToken:', error);
-    }
-}
-
- 
- // Fungsi untuk mendapatkan token dari cookie
- function getTokenFromCookies(cookieName) {
+  // Fungsi untuk mendapatkan token dari cookie
+  function getTokenFromCookies(cookieName) {
     const cookies = document.cookie.split(';');
     for (const cookie of cookies) {
         const [name, value] = cookie.trim().split('=');
@@ -47,52 +10,81 @@ async function getUserReportWithToken() {
     return null;
 }
 
-
-// Fungsi untuk menampilkan data laporan ke dalam elemen HTML
-function displayReportData(reportData) {
-    const reportContainer = document.getElementById('reportContainer');
-    reportContainer.innerHTML = '';
-
-    try {
-        console.log('Displaying data:', reportData);
-
-        if (reportData && reportData.length > 0) {
-            reportData.forEach((report) => {
-                const newCard = document.createElement('div');
-                newCard.className = 'card timeline-card bg-dark';
-                newCard.innerHTML = `
-                    <div class="card-body">
-                        <div class="d-flex justify-content-between">
-                            <div class="timeline-text mb-2">
-                                <h6 class="element-heading fw-bolder">${report.reportid}</h6>
-                                <span>${report.location.locationName}</span>
-                            </div>
-                            <div class="timeline-text mb-2">
-                                <span class="badge mb-2 rounded-pill bg-dark">${report.date}</span>
-                            </div>
-                        </div>
-                        <div class="divider mt-0"></div>
-                        <div class="text-content mb-2">
-                            <h6 class="mb-0">Jenis Ketidaksesuaian</h6>
-                            <div class="timeline-tags">
-                                ${report.typeDangerousActions.map(action => `<span class="badge bg-light text-dark">#${action.typeName}</span>`).join('')}
-                            </div>
-                        </div>
-                        <div class="text-content mb-2">
-                            <h6 class="mb-0">Pengawas</h6>
-                            <p><span>${report.user.nama}</span> <br> <span>${report.user.jabatan}</span></p>
-                        </div>
-                    </div>
-                `;
-                reportContainer.appendChild(newCard);
-            });
-        } else {
-            console.error('Data tidak dapat ditemukan atau tidak sesuai dengan format yang diharapkan');
-            reportContainer.innerHTML = '<p>No report data found.</p>';
-        }
-    } catch (error) {
-        console.error('Error in displayReportData:', error);
+const getUserReportWithToken = async () => {
+    const token = getTokenFromCookies('Login');
+  
+    if (!token) {
+      alert("Token tidak ditemukan");
+      return;
     }
-}
+  
+    const targetURL = 'https://asia-southeast2-ordinal-stone-389604.cloudfunctions.net/GetAllReportbyUser';
+  
+    const myHeaders = new Headers();
+    myHeaders.append('Login', token);
+  
+    const requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      redirect: 'follow',
+    };
+  
+    try {
+      const response = await fetch(targetURL, requestOptions);
+      const data = await response.json();
+  
+      if (data.status === 200) {
+        // Panggil fungsi untuk menampilkan data ke dalam card
+        displayReportData(data.data, 'reportContainer');
+      } else {
+        console.error('Server response:', data.message || 'Data tidak dapat ditemukan');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+ 
+
+
+  const displayReportData = (reportData, cardContainerId) => {
+    const reportContainer = document.getElementById(cardContainerId);
+  
+    reportContainer.innerHTML = '';
+  
+    if (reportData && reportData.length > 0) {
+      reportData.forEach((report) => {
+        const newCard = document.createElement('div');
+        newCard.className = 'card timeline-card bg-dark';
+        newCard.innerHTML = `
+          <div class="card-body">
+            <div class="d-flex justify-content-between">
+              <div class="timeline-text mb-2">
+                <h6 class="element-heading fw-bolder">${report.reportid}</h6>
+                <span>${report.location.locationName}</span>
+              </div>
+              <div class="timeline-text mb-2">
+                <span class="badge mb-2 rounded-pill bg-dark">${report.date}</span>
+              </div>
+            </div>
+            <div class="divider mt-0"></div>
+            <div class="text-content mb-2">
+              <h6 class="mb-0">Jenis Ketidaksesuaian</h6>
+              <div class="timeline-tags">
+                ${report.typeDangerousActions.map(action => `<span class="badge bg-light text-dark">#${action.typeName}</span>`).join('')}
+              </div>
+            </div>
+            <div class="text-content mb-2">
+              <h6 class="mb-0">Pengawas</h6>
+              <p><span>${report.user.nama}</span> <br> <span>${report.user.jabatan}</span></p>
+            </div>
+          </div>
+        `;
+  
+        reportContainer.appendChild(newCard);
+      });
+    } else {
+      reportContainer.innerHTML = '<p>No report data found.</p>';
+    }
+  };
 
 getUserReportWithToken();
