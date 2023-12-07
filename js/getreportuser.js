16ff39c89818ca206533f261784051a28d3903ea
fahiraaa06 <fahira1678@gmail.com>
2023-12-07 18:51:06 +0700

    fixing erorr js

--- js/getreportuser.js
+++ js/getreportuser.js
@@ -1,6 +1,6 @@
 // Fungsi untuk menampilkan satu card baru
 const displayNewReport = (newReport, cardContainerId) => {
-  const reportContainer = document.getElementById(cardContainerId);
+  const newreportContainer = document.getElementById(cardContainerId);
 
   const newCard = document.createElement('div');
   newCard.className = 'card timeline-card bg-dark';
@@ -30,7 +30,48 @@ const displayNewReport = (newReport, cardContainerId) => {
   `;
 
   // Menambahkan card baru ke dalam container
-  reportContainer.prepend(newCard);
+  newreportContainer.prepend(newCard);
+};
+
+const displayReportData = (reportData, cardContainerId) => {
+  const reportContainer = document.getElementById(cardContainerId);
+
+  reportContainer.innerHTML = '';
+
+  if (reportData && reportData.length > 0) {
+    reportData.forEach((report) => {
+      const newCard = document.createElement('div');
+      newCard.className = 'card timeline-card bg-dark';
+      newCard.innerHTML = `
+        <div class="card-body">
+          <div class="d-flex justify-content-between">
+            <div class="timeline-text mb-2">
+              <h6 class="element-heading fw-bolder">${report.reportid}</h6>
+              <span>${report.location.locationName}</span>
+            </div>
+            <div class="timeline-text mb-2">
+              <span class="badge mb-2 rounded-pill bg-dark">${report.date}</span>
+            </div>
+          </div>
+          <div class="divider mt-0"></div>
+          <div class="timeline-text mb-2">
+            <h6 class="mb-0">Jenis Ketidaksesuaian</h6>
+            <div class="timeline-tags">
+              ${report.typeDangerousActions.map(action => `<span class="badge bg-light text-dark">${action.typeName}</span>`).join('')}
+            </div>
+          </div>
+          <div class="timeline-text mb-0">
+            <h6 class="mb-0">Pengawas</h6>
+            <span class="fw-normal">${report.user.nama}</span> <br> <span class="fw-normal">${report.user.jabatan}</span>
+          </div>
+        </div>
+      `;
+
+      reportContainer.appendChild(newCard);
+    });
+  } else {
+    reportContainer.innerHTML = '<p>No report data found.</p>';
+  }
 };
 
 // Function to extract the token from cookies
@@ -76,7 +117,7 @@ const getUserReportWithTokenAndDisplayNew = async () => {
           // Menampilkan satu card baru jika ada data baru
           if (data.data.length > 0) {
               const newReport = data.data[0]; // Misalnya, mengambil data report pertama sebagai contoh
-              displayNewReport(newReport, 'reportContainer');
+              displayNewReport(newReport, 'newreportContainer');
           }
       } else {
           console.error('Server response:', data.message || 'Data tidak dapat ditemukan');
