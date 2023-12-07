2b8939f433986d6a3cb3a29a63449d53070261a8
fahiraaa06 <fahira1678@gmail.com>
2023-12-07 20:13:04 +0700

    perbaiki js report

--- js/getreportuser.js
+++ js/getreportuser.js
@@ -34,11 +34,7 @@ const getUserReportWithToken = async () => {
     const data = await response.json();
 
     if (data.status === 200) {
-      // Panggil fungsi untuk menampilkan data terbaru ke dalam card
-      latestDisplayReportData(data.data, 'latestCardContainer');
-      // Panggil fungsi untuk menampilkan seluruh data ke dalam card
-      displayReportData(data.data, 'reportContainer');
-
+      latestDisplayReportData(data.data, 'latestCardContainer')
     } else {
       console.error('Server response:', data.message || 'Data tidak dapat ditemukan');
     }
@@ -91,45 +87,6 @@ const latestDisplayReportData = (reportData, cardContainerId) => {
   }
 };
 
-const displayReportData = (reportData, cardContainerId) => {
-  const reportContainer = document.getElementById(cardContainerId);
-
-  reportContainer.innerHTML = '';
-
-  if (reportData && reportData.length > 0) {
-    reportData.forEach((report) => {
-      const newCard = document.createElement('div');
-      newCard.className = 'card timeline-card bg-dark';
-      newCard.innerHTML = `
-        <div class="card-body">
-          <div class="d-flex justify-content-between">
-            <div class="timeline-text mb-2">
-              <h6 class="element-heading fw-bolder">${report.reportid}</h6>
-              <span>${report.location.locationName}</span>
-            </div>
-            <div class="timeline-text mb-2">
-              <span class="badge mb-2 rounded-pill bg-dark">${report.date}</span>
-            </div>
-          </div>
-          <div class="divider mt-0"></div>
-          <div class="timeline-text mb-2">
-            <h6 class="mb-0">Jenis Ketidaksesuaian</h6>
-            <div class="timeline-tags">
-              ${report.typeDangerousActions.map(action => `<span class="badge bg-light text-dark">${action.typeName}</span>`).join('')}
-            </div>
-          </div>
-          <div class="timeline-text mb-0">
-            <h6 class="mb-0">Pengawas</h6>
-            <span class="fw-normal">${report.user.nama}</span> <br> <span class="fw-normal">${report.user.jabatan}</span>
-          </div>
-        </div>
-      `;
-
-      reportContainer.appendChild(newCard);
-    });
-  } else {
-    reportContainer.innerHTML = '<p>No report data found.</p>';
-  }
-};
-
 getUserReportWithToken();
+
+
