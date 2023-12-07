c1e9d0a786606532fce637779be96f6e39be3054
fahiraaa06 <fahira1678@gmail.com>
2023-12-07 19:54:34 +0700

    perbaiki js report data

--- js/getreportuser.js
+++ js/getreportuser.js
@@ -1,63 +1,66 @@
-  // Fungsi untuk mendapatkan token dari cookie
-  function getTokenFromCookies(cookieName) {
-    const cookies = document.cookie.split(';');
-    for (const cookie of cookies) {
-        const [name, value] = cookie.trim().split('=');
-        if (name === cookieName) {
-            return value;
-        }
+// Fungsi untuk mendapatkan token dari cookie
+function getTokenFromCookies(cookieName) {
+  const cookies = document.cookie.split(';');
+  for (const cookie of cookies) {
+    const [name, value] = cookie.trim().split('=');
+    if (name === cookieName) {
+      return value;
     }
-    return null;
+  }
+  return null;
 }
 
 const getUserReportWithToken = async () => {
-    const token = getTokenFromCookies('Login');
-  
-    if (!token) {
-      alert("Token tidak ditemukan");
-      return;
-    }
-  
-    const targetURL = 'https://asia-southeast2-ordinal-stone-389604.cloudfunctions.net/GetAllReportbyUser';
-  
-    const myHeaders = new Headers();
-    myHeaders.append('Login', token);
-  
-    const requestOptions = {
-      method: 'POST',
-      headers: myHeaders,
-      redirect: 'follow',
-    };
-  
-    try {
-      const response = await fetch(targetURL, requestOptions);
-      const data = await response.json();
-  
-      if (data.status === 200) {
-        // Panggil fungsi untuk menampilkan data ke dalam card
-        displayReportData(data.data, 'reportContainer');
-      } else {
-        console.error('Server response:', data.message || 'Data tidak dapat ditemukan');
-      }
-    } catch (error) {
-      console.error('Error:', error);
-    }
+  const token = getTokenFromCookies('Login');
+
+  if (!token) {
+    alert("Token tidak ditemukan");
+    return;
+  }
+
+  const targetURL = 'https://asia-southeast2-ordinal-stone-389604.cloudfunctions.net/GetAllReportbyUser';
+
+  const myHeaders = new Headers();
+  myHeaders.append('Login', token);
+
+  const requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    redirect: 'follow',
   };
- 
-  const displayReportData = (reportData, cardContainerId) => {
-    const reportContainer = document.getElementById(cardContainerId);
 
-    reportContainer.innerHTML = '';
+  try {
+    const response = await fetch(targetURL, requestOptions);
+    const data = await response.json();
 
-    if (reportData && reportData.length > 0) {
-        // Mengurutkan data berdasarkan tanggal secara descending
-        reportData.sort((a, b) => new Date(b.date) - new Date(a.date));
+    if (data.status === 200) {
+      // Panggil fungsi untuk menampilkan data terbaru ke dalam card
+      latestDisplayReportData(data.data, 'latestCardContainer');
+      // Panggil fungsi untuk menampilkan seluruh data ke dalam card
+      displayReportData(data.data, 'reportContainer');
 
-        const latestReport = reportData[0]; // Mengambil elemen pertama setelah diurutkan
+    } else {
+      console.error('Server response:', data.message || 'Data tidak dapat ditemukan');
+    }
+  } catch (error) {
+    console.error('Error:', error);
+  }
+};
 
-        const newCard = document.createElement('div');
-        newCard.className = 'card timeline-card bg-dark';
-        newCard.innerHTML = `
+const latestDisplayReportData = (reportData, cardContainerId) => {
+  const latestCardContainer = document.getElementById(cardContainerId);
+
+  latestCardContainer.innerHTML = '';
+
+  if (reportData && reportData.length > 0) {
+    // Mengurutkan data berdasarkan tanggal secara descending
+    reportData.sort((a, b) => new Date(b.date) - new Date(a.date));
+
+    const latestReport = reportData[0]; // Mengambil elemen pertama setelah diurutkan
+
+    const newCard = document.createElement('div');
+    newCard.className = 'card timeline-card bg-dark';
+    newCard.innerHTML = `
           <div class="card-body">
             <div class="d-flex justify-content-between">
               <div class="timeline-text mb-2">
@@ -82,10 +85,51 @@ const getUserReportWithToken = async () => {
           </div>
         `;
 
-        reportContainer.appendChild(newCard);
-    } else {
-        reportContainer.innerHTML = '<p>No report data found.</p>';
-    }
+    latestCardContainer.appendChild(newCard);
+  } else {
+    latestCardContainer.innerHTML = '<p>No report data found.</p>';
+  }
+};
+
+const displayReportData = (reportData, cardContainerId) => {
+  const reportContainer = document.getElementById(cardContainerId);
+
+  reportContainer.innerHTML = '';
+
+  if (reportData && reportData.length > 0) {
+    reportData.forEach((report) => {
+      const newCard = document.createElement('div');
+      newCard.className = 'card timeline-card bg-dark';
+      newCard.innerHTML = `
+        <div class="card-body">
+          <div class="d-flex justify-content-between">
+            <div class="timeline-text mb-2">
+              <h6 class="element-heading fw-bolder">${report.reportid}</h6>
+              <span>${report.location.locationName}</span>
+            </div>
+            <div class="timeline-text mb-2">
+              <span class="badge mb-2 rounded-pill bg-dark">${report.date}</span>
+            </div>
+          </div>
+          <div class="divider mt-0"></div>
+          <div class="timeline-text mb-2">
+            <h6 class="mb-0">Jenis Ketidaksesuaian</h6>
+            <div class="timeline-tags">
+              ${report.typeDangerousActions.map(action => `<span class="badge bg-light text-dark">${action.typeName}</span>`).join('')}
+            </div>
+          </div>
+          <div class="timeline-text mb-0">
+            <h6 class="mb-0">Pengawas</h6>
+            <span class="fw-normal">${report.user.nama}</span> <br> <span class="fw-normal">${report.user.jabatan}</span>
+          </div>
+        </div>
+      `;
+
+      reportContainer.appendChild(newCard);
+    });
+  } else {
+    reportContainer.innerHTML = '<p>No report data found.</p>';
+  }
 };
 
 getUserReportWithToken();
