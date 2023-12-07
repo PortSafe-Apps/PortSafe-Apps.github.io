1ad8649ad39517d001e86d3fa15fa917dcb941ab
fahiraaa06 <fahira1678@gmail.com>
2023-12-07 19:44:23 +0700

    test js

--- js/getreportuser.js
+++ js/getreportuser.js
@@ -1,143 +1,91 @@
-// Fungsi untuk mendapatkan token dari cookie
-function getTokenFromCookies(cookieName) {
-  const cookies = document.cookie.split(';');
-  for (const cookie of cookies) {
-      const [name, value] = cookie.trim().split('=');
-      if (name === cookieName) {
-          return value;
-      }
-  }
-  return null;
+  // Fungsi untuk mendapatkan token dari cookie
+  function getTokenFromCookies(cookieName) {
+    const cookies = document.cookie.split(';');
+    for (const cookie of cookies) {
+        const [name, value] = cookie.trim().split('=');
+        if (name === cookieName) {
+            return value;
+        }
+    }
+    return null;
 }
 
-// Fungsi untuk mendapatkan semua data report dari server
-const getAllReportData = async () => {
-  const token = getTokenFromCookies('Login');
-
-  if (!token) {
+const getUserReportWithToken = async () => {
+    const token = getTokenFromCookies('Login');
+  
+    if (!token) {
       alert("Token tidak ditemukan");
       return;
-  }
-
-  const targetURL = 'https://asia-southeast2-ordinal-stone-389604.cloudfunctions.net/GetAllReportbyUser';
-
-  const myHeaders = new Headers();
-  myHeaders.append('Login', token);
-
-  const requestOptions = {
+    }
+  
+    const targetURL = 'https://asia-southeast2-ordinal-stone-389604.cloudfunctions.net/GetAllReportbyUser';
+  
+    const myHeaders = new Headers();
+    myHeaders.append('Login', token);
+  
+    const requestOptions = {
       method: 'POST',
       headers: myHeaders,
       redirect: 'follow',
-  };
-
-  try {
+    };
+  
+    try {
       const response = await fetch(targetURL, requestOptions);
       const data = await response.json();
-
+  
       if (data.status === 200) {
-          // Panggil fungsi untuk menampilkan semua data ke dalam card
-          displayReportData(data.data, 'reportContainer');
-
-          // Panggil fungsi untuk menampilkan satu data terbaru ke dalam card
-          displayLatestReport(data.data, 'latestCardContainer');
+        // Panggil fungsi untuk menampilkan data ke dalam card
+        displayReportData(data.data, 'reportContainer');
       } else {
-          console.error('Server response:', data.message || 'Data tidak dapat ditemukan');
+        console.error('Server response:', data.message || 'Data tidak dapat ditemukan');
       }
-  } catch (error) {
+    } catch (error) {
       console.error('Error:', error);
-  }
-};
+    }
+  };
+ 
+  const displayReportData = (reportData, cardContainerId) => {
+    const reportContainer = document.getElementById(cardContainerId);
 
-// Fungsi untuk menampilkan seluruh data report dalam card
-const displayReportData = (reportData, cardContainerId) => {
-  const reportContainer = document.getElementById(cardContainerId);
+    reportContainer.innerHTML = '';
 
-  reportContainer.innerHTML = '';
+    if (reportData && reportData.length > 0) {
+        // Mengurutkan data berdasarkan tanggal secara descending
+        reportData.sort((a, b) => new Date(b.date) - new Date(a.date));
 
-  if (reportData && reportData.length > 0) {
-      reportData.forEach((report) => {
-          const newCard = document.createElement('div');
-          newCard.className = 'card timeline-card bg-dark';
-          newCard.innerHTML = `
-            <div class="card-body">
-              <div class="d-flex justify-content-between">
-                <div class="timeline-text mb-2">
-                  <h6 class="element-heading fw-bolder">${report.reportid}</h6>
-                  <span>${report.location.locationName}</span>
-                </div>
-                <div class="timeline-text mb-2">
-                  <span class="badge mb-2 rounded-pill bg-dark">${report.date}</span>
-                </div>
+        const latestReport = reportData[0]; // Mengambil elemen pertama setelah diurutkan
+
+        const newCard = document.createElement('div');
+        newCard.className = 'card timeline-card bg-dark';
+        newCard.innerHTML = `
+          <div class="card-body">
+            <div class="d-flex justify-content-between">
+              <div class="timeline-text mb-2">
+                <h6 class="element-heading fw-bolder">${latestReport.reportid}</h6>
+                <span>${latestReport.location.locationName}</span>
               </div>
-              <div class="divider mt-0"></div>
-              <div class="text-content mb-2">
-                <h6 class="mb-0">Jenis Ketidaksesuaian</h6>
-                <div class="timeline-tags">
-                  ${report.typeDangerousActions.map(action => `<span class="badge bg-light text-dark">${action.typeName}</span>`).join('')}
-                </div>
-              </div>
-              <div class="text-content mb-0">
-                <h6 class="mb-0">Pengawas</h6>
-                <span class="fw-normal">${report.user.nama}</span> <br> <span class="fw-normal">${report.user.jabatan}</span>
+              <div class="timeline-text mb-2">
+                <span class="badge mb-2 rounded-pill bg-dark">${latestReport.date}</span>
               </div>
             </div>
-          `;
+            <div class="divider mt-0"></div>
+            <div class="text-content mb-2">
+              <h6 class="mb-0">Jenis Ketidaksesuaian</h6>
+              <div class="timeline-tags">
+                ${latestReport.typeDangerousActions.map(action => `<span class="badge bg-light text-dark">#${action.typeName}</span>`).join('')}
+              </div>
+            </div>
+            <div class="text-content mb-0">
+              <h6 class="mb-0">Pengawas</h6>
+              <span class="fw-normal">${latestReport.user.nama}</span> <br> <span class="fw-normal">${latestReport.user.jabatan}</span>
+            </div>
+          </div>
+        `;
 
-          reportContainer.appendChild(newCard);
-      });
-  } else {
-      reportContainer.innerHTML = '<p>No report data found.</p>';
-  }
+        reportContainer.appendChild(newCard);
+    } else {
+        reportContainer.innerHTML = '<p>No report data found.</p>';
+    }
 };
 
-// Fungsi untuk menampilkan satu data terbaru dalam card
-const displayLatestReport = (reportData, latestCardContainerId) => {
-  const latestCardContainer = document.getElementById(latestCardContainerId);
-
-  latestCardContainer.innerHTML = '';
-
-  if (reportData && reportData.length > 0) {
-      // Mengurutkan data berdasarkan tanggal secara descending
-      reportData.sort((a, b) => new Date(b.date) - new Date(a.date));
-
-      const latestReport = reportData[0]; // Mengambil elemen pertama setelah diurutkan
-
-      // Konversi tanggal
-      const latestReportDate = new Date(latestReport.date);
-      const formattedLatestDate = latestReportDate.toLocaleDateString();
-
-      const newCard = document.createElement('div');
-      newCard.className = 'card timeline-card bg-dark';
-      newCard.innerHTML = `
-        <div class="card-body">
-          <div class="d-flex justify-content-between">
-            <div class="timeline-text mb-2">
-              <h6 class="element-heading fw-bolder">${latestReport.reportid}</h6>
-              <span>${latestReport.location.locationName}</span>
-            </div>
-            <div class="timeline-text mb-2">
-              <span class="badge mb-2 rounded-pill bg-dark">${formattedLatestDate}</span>
-            </div>
-          </div>
-          <div class="divider mt-0"></div>
-          <div class="text-content mb-2">
-            <h6 class="mb-0">Jenis Ketidaksesuaian</h6>
-            <div class="timeline-tags">
-              ${latestReport.typeDangerousActions.map(action => `<span class="badge bg-light text-dark">#${action.typeName}</span>`).join('')}
-            </div>
-          </div>
-          <div class="text-content mb-0">
-            <h6 class="mb-0">Pengawas</h6>
-            <span class="fw-normal">${latestReport.user.nama}</span> <br> <span class="fw-normal">${latestReport.user.jabatan}</span>
-          </div>
-        </div>
-      `;
-
-      latestCardContainer.appendChild(newCard);
-  } else {
-      latestCardContainer.innerHTML = '<p>No report data found.</p>';
-  }
-};
-
-// Panggil fungsi untuk mendapatkan dan menampilkan data
-getAllReportData();
+getUserReportWithToken();
