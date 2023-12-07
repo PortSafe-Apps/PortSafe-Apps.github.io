a9e6541a275277de360a21cb9b349c93020bd307
fahiraaa06 <fahira1678@gmail.com>
2023-12-07 17:26:35 +0700

    perbaharui tampilan beranda

--- js/getreportuser.js
+++ js/getreportuser.js
@@ -67,15 +67,15 @@ const getUserReportWithToken = async () => {
               </div>
             </div>
             <div class="divider mt-0"></div>
-            <div class="text-content mb-2">
+            <div class="timeline-text mb-2">
               <h6 class="mb-0">Jenis Ketidaksesuaian</h6>
               <div class="timeline-tags">
-                ${report.typeDangerousActions.map(action => `<span class="badge bg-light text-dark">#${action.typeName}</span>`).join('')}
+                ${report.typeDangerousActions.map(action => `<span class="badge bg-light text-dark">${action.typeName}</span>`).join('')}
               </div>
             </div>
-            <div class="text-content mb-0">
+            <div class="timeline-text mb-0">
               <h6 class="mb-0">Pengawas</h6>
-              <p><span>${report.user.nama}</span> <br> <span>${report.user.jabatan}</span></p>
+              <span class="fw-normal">${report.user.nama}</span> <br> <span class="fw-normal">${report.user.jabatan}</span>
             </div>
           </div>
         `;
@@ -86,5 +86,47 @@ const getUserReportWithToken = async () => {
       reportContainer.innerHTML = '<p>No report data found.</p>';
     }
   };
+
+  const displayLatestReport = (reportData, latestCardContainerId) => {
+    const latestCardContainer = document.getElementById(latestCardContainerId);
+
+    latestCardContainer.innerHTML = '';
+
+    if (reportData && reportData.length > 0) {
+        reportData.sort((a, b) => new Date(b.date) - new Date(a.date));
+
+        const latestReport = reportData[0];
+
+        const newCard = document.createElement('div');
+        newCard.className = 'card timeline-card bg-dark';
+        newCard.innerHTML = `
+        <div class="card-body">
+          <div class="d-flex justify-content-between">
+            <div class="timeline-text mb-2">
+              <h6 class="element-heading fw-bolder">${report.reportid}</h6>
+              <span>${report.location.locationName}</span>
+            </div>
+            <div class="timeline-text mb-2">
+              <span class="badge mb-2 rounded-pill bg-dark">${report.date}</span>
+            </div>
+          </div>
+          <div class="divider mt-0"></div>
+          <div class="timeline-text mb-2">
+            <h6 class="mb-0">Jenis Ketidaksesuaian</h6>
+            <div class="timeline-tags">
+              ${report.typeDangerousActions.map(action => `<span class="badge bg-light text-dark">${action.typeName}</span>`).join('')}
+            </div>
+          </div>
+          <div class="timeline-text mb-0">
+            <h6 class="mb-0">Pengawas</h6>
+            <span class="fw-normal">${report.user.nama}</span> <br> <span class="fw-normal">${report.user.jabatan}</span>
+          </div>
+        </div>
+      `;
+        latestCardContainer.appendChild(newCard);
+    } else {
+        latestCardContainer.innerHTML = '<p>No report data found.</p>';
+    }
+};
 
 getUserReportWithToken();
