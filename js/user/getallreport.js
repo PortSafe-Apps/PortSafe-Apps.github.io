57e279fbff54e7836de61da4513b26cb821e526f
fahiraaa06 <fahira1678@gmail.com>
2023-12-10 11:37:42 +0700

    tambah detail report

--- js/user/getallreport.js
+++ js/user/getallreport.js
@@ -1,64 +1,176 @@
 // Fungsi untuk mendapatkan token dari cookie
 function getTokenFromCookies(cookieName) {
-    const cookies = document.cookie.split(';');
-    for (const cookie of cookies) {
-      const [name, value] = cookie.trim().split('=');
-      if (name === cookieName) {
-        return value;
-      }
+  const cookies = document.cookie.split(';');
+  for (const cookie of cookies) {
+    const [name, value] = cookie.trim().split('=');
+    if (name === cookieName) {
+      return value;
     }
-    return null;
+  }
+  return null;
 }
 
-const getallUserReportWithToken = async () => {
-    const token = getTokenFromCookies('Login');
-  
-    if (!token) {
-      Swal.fire({
-        icon: 'warning',
-        title: 'Authentication Error',
-        text: 'Kamu Belum Login!',
-      }).then(() => {
-        window.location.href = 'https://portsafe-apps.github.io/'; 
-      });
-      return;
-    }
-  
-    const targetURL = 'https://asia-southeast2-ordinal-stone-389604.cloudfunctions.net/GetAllReportbyUser';
-  
-    const myHeaders = new Headers();
-    myHeaders.append('Login', token);
-  
-    const requestOptions = {
-      method: 'POST',
-      headers: myHeaders,
-      redirect: 'follow',
-    };
-  
-    try {
-      const response = await fetch(targetURL, requestOptions);
-      const data = await response.json();
-  
-      if (data.status === 200) {
-        displayReportData(data.data, 'reportContainer')
-      } else {
-        console.error('Server response:', data.message || 'Data tidak dapat ditemukan');
-      }
-    } catch (error) {
-      console.error('Error:', error);
-    }
+// Fungsi untuk mengambil laporan berdasarkan ID
+const getDetailedReport = async (reportid) => {
+  const token = getTokenFromCookies('Login');
+
+  if (!token) {
+    // Tangani kesalahan autentikasi jika tidak ada token
+    Swal.fire({
+      icon: 'warning',
+      title: 'Authentication Error',
+      text: 'Kamu Belum Login!',
+    }).then(() => {
+      window.location.href = 'https://portsafe-apps.github.io/';
+    });
+    return;
+  }
+
+  const targetURL = `https://asia-southeast2-ordinal-stone-389604.cloudfunctions.net/oneReport-${reportid}`;
+
+  const myHeaders = new Headers();
+  myHeaders.append('Login', token);
+
+  const requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    redirect: 'follow',
   };
-  
-  const displayReportData = (reportData, cardContainerId) => {
-    const reportContainer = document.getElementById(cardContainerId);
-  
-    reportContainer.innerHTML = '';
-  
-    if (reportData && reportData.length > 0) {
-      reportData.forEach((report) => {
-        const newCard = document.createElement('div');
-        newCard.className = 'card timeline-card bg-dark';
-        newCard.innerHTML = `
+
+  try {
+    const response = await fetch(targetURL, requestOptions);
+    const data = await response.json();
+
+    if (data.status === 200) {
+      // Tampilkan informasi detail laporan
+      displayDetailedReport(data.data);
+    } else {
+      console.error('Server response:', data.message || 'Data tidak dapat ditemukan');
+    }
+  } catch (error) {
+    console.error('Error:', error);
+  }
+};
+
+// Fungsi untuk menampilkan informasi detail laporan ke dalam HTML
+const displayDetailedReport = (detailedReport) => {
+  const detailContainer = document.getElementById('detailContainer');
+
+  detailContainer.innerHTML = '';
+
+  if (detailedReport) {
+    const detailCard = document.createElement('div');
+    detailCard.className = 'card bg-light';
+    detailCard.innerHTML = `
+      <div class="card-body">
+        <h6 class="mb-0">Nomor Pelaporan</h6>
+        <p>${detailedReport.reportid}</p>
+        
+        <h6 class="mb-0">Tanggal Pelaporan</h6>
+        <p>${detailedReport.date}</p>
+        
+        <h6 class="mb-0">Informasi Pengawas</h6>
+        <p class="mb-0">${detailedReport.user.nama}</p>
+        <p>${detailedReport.user.jabatan}</p>
+        
+        <h6 class="mb-0">Lokasi Kejadian</h6>
+        <p>${detailedReport.location.locationName}</p>
+        
+        <h6 class="mb-0">Deskripsi Pengamatan</h6>
+        <p>${detailedReport.description}</p>
+        
+        <h6>Foto Kejadian</h6>
+        <div class="text-center">
+          <img class="w-75 mb-4" src="${detailedReport.observationPhoto}" alt="Foto Kejadian">
+        </div>
+        
+        <h6 class="mb-0">Tindakan Berbahaya yang Dilakukan</h6>
+        <ul class="ps-0 fs-6">
+          ${detailedReport.typeDangerousActions.map((action, index) => `
+            <li><span>${index + 1}.</span> ${action.typeName}</li>
+            <ul class="ps-3">
+              ${action.subTypes.map(subType => `
+                <li><i class="bi bi-dash me-2"></i>${subType}</li>
+              `).join('')}
+            </ul>
+          `).join('')}
+        </ul>
+
+        <h6 class="mb-0">Area</h6>
+        <p>${detailedReport.area.areaName}</p>
+
+        <h6 class="mb-0">Tindakan Perbaikan Segera</h6>
+        <p>${detailedReport.immediateAction}</p>
+
+        <h6>Foto Tindakan Perbaikan</h6>
+        <div class="text-center">
+          <img class="w-75 mb-4" src="${detailedReport.improvementPhoto}" alt="Foto Tindakan Perbaikan">
+        </div>
+
+        <h6 class="mb-0">Tindakan Pencegahan Terulang Kembali</h6>
+        <p>${detailedReport.correctiveAction}</p>
+      </div>
+    `;
+
+    detailContainer.appendChild(detailCard);
+  } else {
+    detailContainer.innerHTML = '<p>Informasi detail tidak ditemukan.</p>';
+  }
+};
+
+// Fungsi untuk mendapatkan semua laporan pengguna dengan token
+const getallUserReportWithToken = async () => {
+  const token = getTokenFromCookies('Login');
+
+  if (!token) {
+    // Tangani kesalahan autentikasi jika tidak ada token
+    Swal.fire({
+      icon: 'warning',
+      title: 'Authentication Error',
+      text: 'Kamu Belum Login!',
+    }).then(() => {
+      window.location.href = 'https://portsafe-apps.github.io/';
+    });
+    return;
+  }
+
+  const targetURL = 'https://asia-southeast2-ordinal-stone-389604.cloudfunctions.net/GetAllReportbyUser';
+
+  const myHeaders = new Headers();
+  myHeaders.append('Login', token);
+
+  const requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    redirect: 'follow',
+  };
+
+  try {
+    const response = await fetch(targetURL, requestOptions);
+    const data = await response.json();
+
+    if (data.status === 200) {
+      // Tampilkan laporan pengguna dalam bentuk kartu
+      displayReportData(data.data, 'reportContainer');
+    } else {
+      console.error('Server response:', data.message || 'Data tidak dapat ditemukan');
+    }
+  } catch (error) {
+    console.error('Error:', error);
+  }
+};
+
+// Fungsi untuk menampilkan laporan pengguna dalam bentuk kartu
+const displayReportData = (reportData, cardContainerId) => {
+  const reportContainer = document.getElementById(cardContainerId);
+
+  reportContainer.innerHTML = '';
+
+  if (reportData && reportData.length > 0) {
+    reportData.forEach((report) => {
+      const newCard = document.createElement('div');
+      newCard.className = 'card timeline-card bg-dark';
+      newCard.innerHTML = `
           <div class="card-body">
             <div class="d-flex justify-content-between">
               <div class="timeline-text mb-2">
@@ -82,12 +194,19 @@ const getallUserReportWithToken = async () => {
             </div>
           </div>
         `;
-  
-        reportContainer.appendChild(newCard);
+
+      // Tambahkan event listener klik pada kartu
+      newCard.addEventListener('click', () => {
+        // Panggil fungsi untuk mengambil dan menampilkan informasi laporan detail
+        getDetailedReport(report.reportid);
       });
-    } else {
-      reportContainer.innerHTML = '<p>No report data found.</p>';
-    }
-  };
-  
-  getallUserReportWithToken();
+
+      reportContainer.appendChild(newCard);
+    });
+  } else {
+    reportContainer.innerHTML = '<p>No report data found.</p>';
+  }
+};
+
+// Panggil fungsi untuk mendapatkan dan menampilkan laporan pengguna
+getallUserReportWithToken();
