f6dd190fdd33fddebe13db5abd782c34dc5a2093
fahiraaa06 <fahira1678@gmail.com>
2023-12-10 22:25:26 +0700

    perbaikan foto

--- js/user/addreport.js
+++ js/user/addreport.js
@@ -1,10 +1,10 @@
 const getTokenFromCookies = (cookieName) => {
   const cookies = document.cookie.split(';');
   for (const cookie of cookies) {
-      const [name, value] = cookie.trim().split('=');
-      if (name === cookieName) {
-          return value;
-      }
+    const [name, value] = cookie.trim().split('=');
+    if (name === cookieName) {
+      return value;
+    }
   }
   return null;
 };
@@ -38,47 +38,50 @@ const insertObservationReport = async (event) => {
     function getCheckedCheckboxes() {
       var checkboxes = document.querySelectorAll('#checkboxContainer input[type="checkbox"]:checked');
       var checkedValues = [];
-  
+
       checkboxes.forEach(function (checkbox) {
-          var typeId = checkbox.name;
-          var typeName = checkbox.dataset.typeName; // Mengambil Type Name dari dataset
-  
-          checkedValues.push({
-              TypeId: typeId,
-              TypeName: typeName,
-              SubTypes: [checkbox.value]
-          });
+        var typeId = checkbox.name;
+        var typeName = checkbox.dataset.typeName; // Mengambil Type Name dari dataset
+
+        checkedValues.push({
+          TypeId: typeId,
+          TypeName: typeName,
+          SubTypes: [checkbox.value]
+        });
       });
-  
+
       return checkedValues;
-  }  
-  
+    }
+
+    const fotoObservasiBase64 = await getBase64FromImage('hasilFotoObservasi');
+    const fotoPerbaikanBase64 = await getBase64FromImage('hasilFotoPerbaikan');
+    
     const requestOptions = {
       method: 'POST',
       headers: myHeaders,
       body: JSON.stringify({
-          Reportid: document.getElementById('nomorPelaporan').value,
-          Date: document.getElementById('tanggalPelaporan').value,
-          User: {},
-          Location: {
-              LocationName: document.getElementById('autoCompleteLocation').value,
-          },
-          Description: document.getElementById('deskripsiPengamatan').value,
-          ObservationPhoto: document.getElementById('hasilFotoObservasi').value, 
-          TypeDangerousActions: getCheckedCheckboxes(),
-          Area: {
-              AreaName: document.getElementById('newAreaName').value,
-          },
-          ImmediateAction: document.getElementById('deskripsiPerbaikanSegera').value,
-          ImprovementPhoto: document.getElementById('hasilFotoPerbaikan').value,  
-          CorrectiveAction: document.getElementById('deskripsiPencegahanTerulangKembali').value,
+        Reportid: document.getElementById('nomorPelaporan').value,
+        Date: document.getElementById('tanggalPelaporan').value,
+        User: {},
+        Location: {
+          LocationName: document.getElementById('autoCompleteLocation').value,
+        },
+        Description: document.getElementById('deskripsiPengamatan').value,
+        ObservationPhoto: fotoObservasiBase64,
+        TypeDangerousActions: getCheckedCheckboxes(),
+        Area: {
+          AreaName: document.getElementById('newAreaName').value,
+        },
+        ImmediateAction: document.getElementById('deskripsiPerbaikanSegera').value,
+        ObservationPhoto: fotoPerbaikanBase64,
+        CorrectiveAction: document.getElementById('deskripsiPencegahanTerulangKembali').value,
       }),
       redirect: 'follow',
-  };
-  
+    };
+
     const response = await fetch(targetURL, requestOptions);
     const data = await response.json();
-  
+
     if (data.status === false) {
       showAlert(data.message, 'error');
     } else {
@@ -87,10 +90,36 @@ const insertObservationReport = async (event) => {
     }
   } catch (error) {
     console.error('Error:', error);
-  
+
+  }
+};
+
+// Fungsi untuk mengambil base64 dari elemen gambar
+async function getBase64FromImage(elementId) {
+  const imageElement = document.getElementById(elementId);
+
+  return new Promise((resolve, reject) => {
+    const canvas = document.createElement('canvas');
+    const ctx = canvas.getContext('2d');
+    const image = new Image();
+
+    image.onload = function () {
+      canvas.width = image.width;
+      canvas.height = image.height;
+      ctx.drawImage(image, 0, 0, image.width, image.height);
+
+      const dataURL = canvas.toDataURL('image/png'); // Ganti format jika diperlukan
+      resolve(dataURL.replace(/^data:image\/(png|jpg);base64,/, ''));
+    };
+
+    image.onerror = function (error) {
+      reject(error);
+    };
+
+    image.src = imageElement.src;
+  });
 }
-  };
-  
-  document.getElementById('newReportForm').addEventListener('submit', insertObservationReport);
+
+document.getElementById('newReportForm').addEventListener('submit', insertObservationReport);
 
 
