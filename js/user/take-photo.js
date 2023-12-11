dcc4f574be1374b1369c03b1a5333923df32ea14
fahiraaa06 <fahira1678@gmail.com>
2023-12-11 22:05:24 +0700

    take photo js 3

--- js/user/take-photo.js
+++ js/user/take-photo.js
@@ -14,27 +14,17 @@ function ambilFotoObservasi() {
 document.getElementById('fotoObservasi').addEventListener('change', ambilFotoObservasi);
 
 
-
 const inputFotoPerbaikan = document.getElementById('fotoPerbaikan');
 const imgHasilFotoPerbaikan = document.getElementById('hasilFotoPerbaikan');
 
 function ambilFotoPerbaikan() {
     const fileInput = inputFotoPerbaikan.files[0];
-    const reader = new FileReader();
 
-    reader.onloadend = function () {
-        const fotoPerbaikanBase64 = reader.result;
+    // Set the source of the image using URL.createObjectURL
+    imgHasilFotoPerbaikan.src = URL.createObjectURL(fileInput);
 
-        // Set the source of the image directly
-        imgHasilFotoPerbaikan.src = fotoPerbaikanBase64;
-
-        // You can use fotoPerbaikanBase64 in other ways (e.g., send to the server)
-        console.log(fotoPerbaikanBase64);
-    };
-
-    if (fileInput) {
-        reader.readAsDataURL(fileInput);
-    }
+    // You can use fileInput in other ways (e.g., send to the server)
+    console.log(fileInput);
 }
 
 document.getElementById('fotoPerbaikan').addEventListener('change', ambilFotoPerbaikan);
