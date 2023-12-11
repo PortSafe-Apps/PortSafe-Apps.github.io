86e23228bd98e2f18e758235a0efbc5119c0c064
fahiraaa06 <fahira1678@gmail.com>
2023-12-11 19:18:42 +0700

    perbaikan get report detail

--- js/user/report.js
+++ js/user/report.js
@@ -10,8 +10,8 @@ function getTokenFromCookies(cookieName) {
   return null;
 }
 
-// Fungsi untuk mendapatkan semua laporan pengguna dengan token
-const getAllUserReport = async () => {
+// Fungsi untuk menampilkan informasi detail laporan ke dalam HTML
+const displayDetailedReport = async (reportid, detailContainerId) => {
   const token = getTokenFromCookies('Login');
 
   if (!token) {
@@ -26,7 +26,7 @@ const getAllUserReport = async () => {
     return;
   }
 
-  const targetURL = 'https://asia-southeast2-ordinal-stone-389604.cloudfunctions.net/GetAllReportbyUser';
+  const targetURL = `https://asia-southeast2-ordinal-stone-389604.cloudfunctions.net/oneReport-1`;
 
   const myHeaders = new Headers();
   myHeaders.append('Login', token);
@@ -34,6 +34,7 @@ const getAllUserReport = async () => {
   const requestOptions = {
     method: 'POST',
     headers: myHeaders,
+    body: JSON.stringify({ reportid }), // Pass reportid in the request body
     redirect: 'follow',
   };
 
@@ -42,8 +43,70 @@ const getAllUserReport = async () => {
     const data = await response.json();
 
     if (data.status === 200) {
-      // Tampilkan laporan pengguna dalam bentuk kartu
-      displayReportData(data.data, 'reportContainer');
+      // Tampilkan informasi detail laporan
+      const detailContainer = document.getElementById(detailContainerId);
+
+      if (!detailContainer) {
+        console.error(`Error: Element with ID "${detailContainerId}" not found.`);
+        return;
+      }
+
+      detailContainer.innerHTML = '';
+
+      const detailCard = document.createElement('div');
+      detailCard.className = 'card';
+      detailCard.innerHTML = `
+        <div class="card-body">
+          <h6 class="mb-0">Nomor Pelaporan</h6>
+          <p>${detailContainer.reportid}</p>
+          
+          <h6 class="mb-0">Tanggal Pelaporan</h6>
+          <p>${detailContainer.date}</p>
+          
+          <h6 class="mb-0">Informasi Pengawas</h6>
+          <p class="mb-0">${detailContainer.user.nama}</p>
+          <p>${detailContainer.user.jabatan}</p>
+          
+          <h6 class="mb-0">Lokasi Kejadian</h6>
+          <p>${detailContainer.location.locationName}</p>
+          
+          <h6 class="mb-0">Deskripsi Pengamatan</h6>
+          <p>${detailContainer.description}</p>
+          
+          <h6>Foto Kejadian</h6>
+          <div class="text-center">
+            <img class="w-75 mb-4" src="${detailContainer.observationPhoto}" alt="Foto Kejadian">
+          </div>
+          
+          <h6 class="mb-0">Tindakan Berbahaya yang Dilakukan</h6>
+          <ul class="ps-0 fs-6">
+            ${detailContainer.typeDangerousActions.map((action, index) => `
+              <li><span>${index + 1}.</span> ${action.typeName}</li>
+              <ul class="ps-3">
+                ${action.subTypes.map(subType => `
+                  <li><i class="bi bi-dash me-2"></i>${subType}</li>
+                `).join('')}
+              </ul>
+            `).join('')}
+          </ul>
+  
+          <h6 class="mb-0">Area</h6>
+          <p>${detailContainer.area.areaName}</p>
+  
+          <h6 class="mb-0">Tindakan Perbaikan Segera</h6>
+          <p>${detailContainer.immediateAction}</p>
+  
+          <h6>Foto Tindakan Perbaikan</h6>
+          <div class="text-center">
+            <img class="w-75 mb-4" src="${detailContainer.improvementPhoto}" alt="Foto Tindakan Perbaikan">
+          </div>
+  
+          <h6 class="mb-0">Tindakan Pencegahan Terulang Kembali</h6>
+          <p>${detailContainer.correctiveAction}</p>
+        </div>
+      `;
+
+      detailContainer.appendChild(detailCard);
     } else {
       console.error('Server response:', data.message || 'Data tidak dapat ditemukan');
     }
@@ -53,7 +116,7 @@ const getAllUserReport = async () => {
 };
 
 // Fungsi untuk menampilkan laporan pengguna dalam bentuk kartu
-const displayReportData = (reportData, cardContainerId) => {
+const displayReportData = (reportData, cardContainerId, detailPageURL) => {
   const reportContainer = document.getElementById(cardContainerId);
 
   reportContainer.innerHTML = '';
@@ -87,16 +150,14 @@ const displayReportData = (reportData, cardContainerId) => {
         </div>
       `;
 
+
       // Tambahkan event listener klik pada kartu
       newCard.addEventListener('click', () => {
         // Ambil reportid dari objek report
         const reportid = report.reportid;
 
-        // Navigasi ke halaman baru dengan menyertakan reportid sebagai parameter query
-        window.location.href = `https://portsafe-apps.github.io/pages/user/detailreport.html?reportid=${reportid}`;
-
-        // Panggil fungsi getDetailedReport untuk mendapatkan dan menampilkan laporan detail
-        getDetailedReport(reportid);
+        // Navigasi ke halaman detail dengan menyertakan reportid sebagai parameter query
+        window.location.href = `${detailPageURL}?reportid=${reportid}`;
       });
 
       // Tambahkan kartu ke kontainer laporan
@@ -110,120 +171,21 @@ const displayReportData = (reportData, cardContainerId) => {
 // Panggil fungsi untuk mendapatkan dan menampilkan laporan pengguna
 getAllUserReport();
 
-// Fungsi untuk menampilkan informasi detail laporan ke dalam HTML
-const displayDetailedReport = (detailedReport) => {
-  const detailContainer = document.getElementById('detailContainer');
+// ID elemen target di halaman detail report
+const detailContainerId = 'detailContainer';
 
-  // Tambahkan pengecekan apakah detailContainer ditemukan
-  if (!detailContainer) {
-    console.error('Error: Element with ID "detailContainer" not found.');
-    return;
-  }
+// ID elemen target di halaman list report
+const cardContainerId = 'reportContainer';
 
-  detailContainer.innerHTML = '';
+// URL halaman detail report
+const detailPageURL = 'https://portsafe-apps.github.io/pages/user/detailreport.html';
 
-  if (detailedReport) {
-    const detailCard = document.createElement('div');
-    detailCard.className = 'card';
-    detailCard.innerHTML = `
-      <div class="card-body">
-        <h6 class="mb-0">Nomor Pelaporan</h6>
-        <p>${detailedReport.reportid}</p>
-        
-        <h6 class="mb-0">Tanggal Pelaporan</h6>
-        <p>${detailedReport.date}</p>
-        
-        <h6 class="mb-0">Informasi Pengawas</h6>
-        <p class="mb-0">${detailedReport.user.nama}</p>
-        <p>${detailedReport.user.jabatan}</p>
-        
-        <h6 class="mb-0">Lokasi Kejadian</h6>
-        <p>${detailedReport.location.locationName}</p>
-        
-        <h6 class="mb-0">Deskripsi Pengamatan</h6>
-        <p>${detailedReport.description}</p>
-        
-        <h6>Foto Kejadian</h6>
-        <div class="text-center">
-          <img class="w-75 mb-4" src="${detailedReport.observationPhoto}" alt="Foto Kejadian">
-        </div>
-        
-        <h6 class="mb-0">Tindakan Berbahaya yang Dilakukan</h6>
-        <ul class="ps-0 fs-6">
-          ${detailedReport.typeDangerousActions.map((action, index) => `
-            <li><span>${index + 1}.</span> ${action.typeName}</li>
-            <ul class="ps-3">
-              ${action.subTypes.map(subType => `
-                <li><i class="bi bi-dash me-2"></i>${subType}</li>
-              `).join('')}
-            </ul>
-          `).join('')}
-        </ul>
+// Fungsi untuk menampilkan laporan pengguna dalam bentuk kartu
+displayReportData(data, cardContainerId, detailPageURL);
 
-        <h6 class="mb-0">Area</h6>
-        <p>${detailedReport.area.areaName}</p>
+// Ambil reportid dari parameter query
+const reportid = new URLSearchParams(window.location.search).get('reportid');
 
-        <h6 class="mb-0">Tindakan Perbaikan Segera</h6>
-        <p>${detailedReport.immediateAction}</p>
-
-        <h6>Foto Tindakan Perbaikan</h6>
-        <div class="text-center">
-          <img class="w-75 mb-4" src="${detailedReport.improvementPhoto}" alt="Foto Tindakan Perbaikan">
-        </div>
-
-        <h6 class="mb-0">Tindakan Pencegahan Terulang Kembali</h6>
-        <p>${detailedReport.correctiveAction}</p>
-      </div>
-    `;
-
-    detailContainer.appendChild(detailCard);
-  } else {
-    detailContainer.innerHTML = '<p>Informasi detail tidak ditemukan.</p>';
-  }
-};
-
-
-
-// Fungsi untuk mendapatkan laporan detail dan navigasi ke halaman baru saat card diklik
-const getDetailedReport = async (reportid) => {
-  const token = getTokenFromCookies('Login');
-
-  if (!token) {
-    // Tangani kesalahan autentikasi jika tidak ada token
-    Swal.fire({
-      icon: 'warning',
-      title: 'Authentication Error',
-      text: 'Kamu Belum Login!',
-    }).then(() => {
-      window.location.href = 'https://portsafe-apps.github.io/';
-    });
-    return;
-  }
-
-  const targetURL = `https://asia-southeast2-ordinal-stone-389604.cloudfunctions.net/oneReport-1`;
-
-  const myHeaders = new Headers();
-  myHeaders.append('Login', token);
-
-  const requestOptions = {
-    method: 'POST',
-    headers: myHeaders,
-    body: JSON.stringify({ reportid }), // Pass reportid in the request body
-    redirect: 'follow',
-  };
-
-  try {
-    const response = await fetch(targetURL, requestOptions);
-    const data = await response.json();
-
-    if (data.status === 200) {
-      // Tampilkan informasi detail laporan
-      displayDetailedReport(data.data);
-    } else {
-      console.error('Server response:', data.message || 'Data tidak dapat ditemukan');
-    }
-  } catch (error) {
-    console.error('Error:', error);
-  }
-};
+// Panggil fungsi getDetailedReport untuk mendapatkan dan menampilkan laporan detail
+displayDetailedReport(reportid, detailContainerId);
 
