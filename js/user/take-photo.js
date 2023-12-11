5b2c2a5f28033e0a766e6ab31bd1c1626471be70
fahiraaa06 <fahira1678@gmail.com>
2023-12-11 15:10:23 +0700

    perbaikan takephoto

--- js/user/take-photo.js
+++ js/user/take-photo.js
@@ -1,26 +1,25 @@
 const inputFotoObservasi = document.getElementById('fotoObservasi');
 const imgHasilFotoObservasi = document.getElementById('hasilFotoObservasi');
+let fotoObservasiDataURL = null; // Variabel untuk menyimpan URL data gambar
 
 function ambilFotoObservasi() {
     const fileInput = inputFotoObservasi.files[0];
     const reader = new FileReader();
 
     reader.onloadend = function () {
-        const fotoObservasiBase64 = reader.result;
+        fotoObservasiDataURL = reader.result;
 
         // Set the source of the image directly
-        imgHasilFotoObservasi.src = fotoObservasiBase64;
+        imgHasilFotoObservasi.src = fotoObservasiDataURL;
 
-        // You can use fotoObservasiBase64 in other ways (e.g., send to the server)
-        console.log(fotoObservasiBase64);
+        // You can use fotoObservasiDataURL in other ways (e.g., send to the server)
+        console.log(fotoObservasiDataURL);
     };
 
     if (fileInput) {
         reader.readAsDataURL(fileInput);
     }
 }
-
-document.getElementById('fotoObservasi').addEventListener('change', ambilFotoObservasi);
 
 
 
