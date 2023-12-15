66c1950704fb88ec02c9fd6e987d0f75fc50dedc
fahiraaa06 <fahira1678@gmail.com>
2023-12-15 13:44:23 +0700

    perbaikan modified photo

--- js/user/takephoto.js
+++ js/user/takephoto.js
@@ -1,53 +1,10 @@
 const inputFotoObservasi = document.getElementById('fotoObservasi');
 const imgHasilFotoObservasi = document.getElementById('hasilFotoObservasi');
-
-function ambilFotoObservasi() {
-    const fileInput = inputFotoObservasi.files[0];
-
-    if (fileInput) {
-        const formData = new FormData();
-        formData.append('image', fileInput);
-
-        fetch('https://asia-southeast2-ordinal-stone-389604.cloudfunctions.net/uploadImage', {
-            method: 'POST',
-            body: formData
-        })
-        .then(response => response.text())  // Mengambil teks dari respons
-        .then(data => {
-            // Menampilkan respons di console (opsional)
-            console.log("Server Response:", data);
-
-            // Menampilkan respons ke pengguna (misalnya, sebagai pemberitahuan di halaman web)
-            alert(data);
-
-            // Jika respons berisi URL gambar, Anda dapat mengekstraknya dan menetapkan sebagai sumber gambar
-            const imageUrl = extractImageUrlFromResponse(data);
-            if (imageUrl) {
-                imgHasilFotoObservasi.src = imageUrl;
-            }
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
-    }
-}
-
-// Fungsi untuk mengekstrak URL gambar dari respons teks
-function extractImageUrlFromResponse(responseText) {
-    const urlMatch = responseText.match(/URL publik: (\S+)/);
-    return urlMatch ? urlMatch[1] : null;
-}
-
-// Menambahkan event listener untuk memanggil fungsi ambilFotoObservasi saat ada perubahan pada input file
-inputFotoObservasi.addEventListener('change', ambilFotoObservasi);
-
-
-
 const inputFotoPerbaikan = document.getElementById('fotoPerbaikan');
 const imgHasilFotoPerbaikan = document.getElementById('hasilFotoPerbaikan');
 
-function ambilFotoPerbaikan() {
-    const fileInput = inputFotoPerbaikan.files[0];
+function ambilFoto(inputElement, imgElement) {
+    const fileInput = inputElement.files[0];
 
     if (fileInput) {
         const formData = new FormData();
@@ -57,18 +14,14 @@ function ambilFotoPerbaikan() {
             method: 'POST',
             body: formData
         })
-        .then(response => response.text())  // Mengambil teks dari respons
+        .then(response => response.text())
         .then(data => {
-            // Menampilkan respons di console (opsional)
-            console.log("Server Response:", data);
+            const imageUrl = extractImageUrl(data);
 
-            // Menampilkan respons ke pengguna (misalnya, sebagai pemberitahuan di halaman web)
-            alert(data);
-
-            // Jika respons berisi URL gambar, Anda dapat mengekstraknya dan menetapkan sebagai sumber gambar
-            const imageUrl = extractImageUrlFromResponse(data);
             if (imageUrl) {
-                imgHasilFotoPerbaikan.src = imageUrl;
+                tampilkanGambarDariUrl(imageUrl, imgElement);
+            } else {
+                console.error('Tidak dapat menemukan URL gambar dalam respons server.');
             }
         })
         .catch(error => {
@@ -77,12 +30,25 @@ function ambilFotoPerbaikan() {
     }
 }
 
-// Fungsi untuk mengekstrak URL gambar dari respons teks
-function extractImageUrlFromResponse(responseText) {
-    const urlMatch = responseText.match(/URL publik: (\S+)/);
-    return urlMatch ? urlMatch[1] : null;
+function extractImageUrl(responseText) {
+    const urlPattern = /URL publik: (https:\/\/[^ ]+)/;
+    const match = responseText.match(urlPattern);
+
+    if (match && match[1]) {
+        return match[1];
+    } else {
+        return null;
+    }
 }
 
-// Menambahkan event listener untuk memanggil fungsi ambilFotoPerbaikan saat ada perubahan pada input file
-inputFotoPerbaikan.addEventListener('change', ambilFotoPerbaikan);
+function tampilkanGambarDariUrl(imageUrl, imgElement) {
+    imgElement.src = imageUrl;
+}
 
+inputFotoObservasi.addEventListener('change', function() {
+    ambilFoto(inputFotoObservasi, imgHasilFotoObservasi);
+});
+
+inputFotoPerbaikan.addEventListener('change', function() {
+    ambilFoto(inputFotoPerbaikan, imgHasilFotoPerbaikan);
+});
