fc6013cfe4e19c5b2ab905c321eeeae28a937d60
fahiraaa06 <fahira1678@gmail.com>
2023-12-17 16:42:50 +0700

    fixing latest report

--- js/user/latestreport.js
+++ js/user/latestreport.js
@@ -1,3 +1,16 @@
+// Fungsi untuk mendapatkan token dari cookie
+function getTokenFromCookies(cookieName) {
+  const cookies = document.cookie.split(';');
+  for (const cookie of cookies) {
+    const [name, value] = cookie.trim().split('=');
+    if (name === cookieName) {
+      return value;
+    }
+  }
+  return null;
+}
+
+
 // Fungsi untuk mendapatkan laporan pengguna terbaru tanpa pengurutan
 const getLatestReport = async () => {
   const token = getTokenFromCookies('Login');
