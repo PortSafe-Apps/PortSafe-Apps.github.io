cbfbbfee9ba3b198c4e51b0e859e2b93c8c17a80
fahiraaa06 <fahira1678@gmail.com>
2023-12-22 00:52:09 +0700

    modified chart area

--- assets/js/user/monthly-report.js
+++ assets/js/user/monthly-report.js
@@ -107,189 +107,71 @@ const createApexChart = (chartId, chartOptions, clickCallback) => {
   try {
     const options = {
       chart: {
-        height: 240,
-        type: "area",
-        animations: {
+        height: chartOptions.chart?.height || 240,
+        type: chartOptions.chart.type || {},
+        animations: chartOptions.chart?.animations || {
           enabled: true,
           easing: "easeinout",
           speed: 1000,
         },
-        dropShadow: {
-          enabled: true,
-          opacity: 0.1,
-          blur: 1,
-          left: -5,
-          top: 18,
+        dropShadow: chartOptions.chart?.dropShadow || {
+            enabled: true,
+            opacity: 0.1,
+            blur: 1,
+            left: -5,
+            top: 18,
         },
-        zoom: {
+        zoom: chartOptions.chart?.zoom || {
           enabled: false,
         },
-        toolbar: {
+        toolbar: chartOptions.chart?.toolbar || {
           show: false,
         },
       },
-      colors: ["#02172C"],
-      dataLabels: {
-        enabled: false,
-      },
-      fill: {
-        type: "gradient",
-        gradient: {
-          type: "vertical",
-          shadeIntensity: 1,
-          inverseColors: true,
-          opacityFrom: 0.15,
-          opacityTo: 0.02,
-          stops: [40, 100],
-        },
-      },
-      grid: {
-        borderColor: "#dbeaea",
-        strokeDashArray: 4,
-        xaxis: {
-          lines: {
-            show: true,
-          },
-        },
-        yaxis: {
-          lines: {
-            show: false,
-          },
-        },
-        padding: {
-          top: 0,
-          right: 0,
-          bottom: 0,
-          left: 0,
-        },
-      },
-      legend: {
-        position: "bottom",
-        horizontalAlign: "center",
-        offsetY: 4,
-        fontSize: "14px",
-        markers: {
-          width: 9,
-          height: 9,
-          strokeWidth: 0,
-          radius: 20,
-        },
-        itemMargin: {
-          horizontal: 5,
-          vertical: 0,
-        },
-      },
-      tooltip: {
-        backgroundColor: "rgb(255,255,255)",
-        bodyFontColor: "#858796",
-        titleMarginBottom: 10,
-        titleFontColor: "#6e707e",
-        titleFontSize: 14,
-        borderColor: "#dddfeb",
-        borderWidth: 1,
-        xPadding: 20,
-        yPadding: 15,
-        displayColors: false,
-        intersect: false,
-        mode: "index",
-        caretPadding: 10,
-        custom: function ({ series, dataPointIndex }) {
-          const month = options.xaxis.categories[dataPointIndex] || "";
-          const value = series[0]?.[dataPointIndex] || 0;
-          return (
-            '<div style="width: 135px; height: 45px;">' +
-            "<span>" +
-            month +
-            "</span>" +
-            "<br>" +
-            "<span>" +
-            "Jumlah Laporan : " +
-            value +
-            "</span>" +
-            "</div>"
-          );
-        },
-      },
-      subtitle: {
-        text: "Tren Jumlah Pelanggaran Setiap Bulan",
-        align: "left",
-        margin: 0,
-        offsetX: 0,
-        offsetY: 0,
-        floating: false,
-        style: {
-          fontSize: "15px",
-          color: "text-dark",
-          fontWeight: "bold",
-          marginBottom: "1rem",
-          fontFamily: "Poppins",
-        },
-      },
-      stroke: {
-        show: true,
-        curve: "smooth",
-        width: 3,
-      },
-      xaxis: {
-        labels: {
-          offsetX: 0,
-          offsetY: 0,
-          style: {
-            colors: "#8480ae",
-            fontSize: "12px",
-            fontFamily: "Poppins",
-          },
-        },
-        tooltip: {
-          enabled: false,
-        },
-      },
-      yaxis: {
-        labels: {
-          offsetX: -10,
-          offsetY: 0,
-          style: {
-            colors: "#8480ae",
-            fontSize: "12px",
-            fontFamily: "Poppins",
-          },
-        },
-      },
+      series: chartOptions.series || [],
+      xaxis: chartOptions.xaxis || {},
+      yaxis: chartOptions.yaxis || {},
+      plotOptions: chartOptions.plotOptions || {},
+      colors: chartOptions.colors || [],
+      padding: chartOptions.padding || {},
+      grid: chartOptions.grid || {},
+      fill: chartOptions.fill || {},
+      legend: chartOptions.legend || {},
+      tooltip: chartOptions.tooltip || {},
+      subtitle: chartOptions.subtitle || {},
+      dataLabels: chartOptions.dataLabels || {},
     };
 
-  // Menggabungkan opsi grafik yang diberikan dengan opsi default
-  const mergedOptions = { ...options, ...chartOptions };
+    const chart = new ApexCharts(document.getElementById(chartId), options);
+    chart.render();
 
-  const chart = new ApexCharts(document.getElementById(chartId), mergedOptions);
-  chart.render();
+    if (chartOptions.xaxis && chartOptions.xaxis.categories) {
+      document.getElementById(chartId).addEventListener("click", function () {
+        try {
+          if (
+            chart &&
+            chart.w &&
+            chart.w.globals &&
+            chart.w.globals.selectedDataPoints
+          ) {
+            const selectedDataPoints = chart.w.globals.selectedDataPoints;
 
-  if (mergedOptions.xaxis && mergedOptions.xaxis.categories) {
-    document.getElementById(chartId).addEventListener("click", function () {
-      try {
-        if (
-          chart &&
-          chart.w &&
-          chart.w.globals &&
-          chart.w.globals.selectedDataPoints
-        ) {
-          const selectedDataPoints = chart.w.globals.selectedDataPoints;
+            if (selectedDataPoints && selectedDataPoints.length > 0) {
+              const clickedIndex = selectedDataPoints[0].dataPointIndex;
 
-          if (selectedDataPoints && selectedDataPoints.length > 0) {
-            const clickedIndex = selectedDataPoints[0].dataPointIndex;
-
-            if (clickCallback) {
-              clickCallback(clickedIndex);
+              if (clickCallback) {
+                clickCallback(clickedIndex);
+              }
             }
           }
+        } catch (error) {
+          console.error("Error handling click event:", error);
         }
-      } catch (error) {
-        console.error("Error handling click event:", error);
-      }
-    });
+      });
+    }
+  } catch (error) {
+    console.error(`Error creating ApexChart for ${chartId}:`, error);
   }
-} catch (error) {
-  console.error(`Error creating ApexChart for ${chartId}:`, error);
-}
 };
 
 // Fungsi untuk mendapatkan jumlah laporan per bulan
@@ -637,7 +519,7 @@ const allChartData = {
   monthly: {
     chartData: {
       chart: {
-        type: "line",
+        type: "area",
       },
       series: [
         {
@@ -648,9 +530,154 @@ const allChartData = {
       xaxis: {
         categories: Array.from(new Array(12), (_, i) => monthToLabel(i)),
       },
+      animations: {
+        enabled: true,
+        easing: "easeinout",
+        speed: 1000,
+      },
+      dropShadow: {
+        enabled: true,
+        opacity: 0.1,
+        blur: 1,
+        left: -5,
+        top: 18,
+      },
+      zoom: {
+        enabled: false,
+      },
+      toolbar: {
+        show: false,
+      },
     },
+    colors: ["#02172C"],
+    dataLabels: {
+      enabled: false,
+    },
+    fill: {
+      type: "gradient",
+      gradient: {
+        type: "vertical",
+        shadeIntensity: 1,
+        inverseColors: true,
+        opacityFrom: 0.15,
+        opacityTo: 0.02,
+        stops: [40, 100],
+      },
+    },
+    grid: {
+      borderColor: "#dbeaea",
+      strokeDashArray: 4,
+      xaxis: {
+        lines: {
+          show: true,
+        },
+      },
+      yaxis: {
+        lines: {
+          show: false,
+        },
+      },
+      padding: {
+        top: 0,
+        right: 0,
+        bottom: 0,
+        left: 0,
+      },
+    },
+    legend: {
+      position: "bottom",
+      horizontalAlign: "center",
+      offsetY: 4,
+      fontSize: "14px",
+      markers: {
+        width: 9,
+        height: 9,
+        strokeWidth: 0,
+        radius: 20,
+      },
+      itemMargin: {
+        horizontal: 5,
+        vertical: 0,
+      },
+    },
+    
+    },
+    subtitle: {
+      text: "Tren Jumlah Pelanggaran Setiap Bulan",
+      align: "left",
+      margin: 0,
+      offsetX: 0,
+      offsetY: 0,
+      floating: false,
+      style: {
+        fontSize: "15px",
+        color: "text-dark",
+        fontWeight: "bold",
+        marginBottom: "1rem",
+        fontFamily: "Poppins",
+      },
+    },
+    tooltip: {
+      backgroundColor: "rgb(255,255,255)",
+      bodyFontColor: "#858796",
+      titleMarginBottom: 10,
+      titleFontColor: "#6e707e",
+      titleFontSize: 14,
+      borderColor: "#dddfeb",
+      borderWidth: 1,
+      xPadding: 20,
+      yPadding: 15,
+      displayColors: false,
+      intersect: false,
+      mode: "index",
+      caretPadding: 10,
+      custom: function ({ series, dataPointIndex }) {
+        const month = options.xaxis.categories[dataPointIndex] || "";
+        const value = series[0]?.[dataPointIndex] || 0;
+        return (
+          '<div style="width: 135px; height: 45px;">' +
+          "<span>" +
+          month +
+          "</span>" +
+          "<br>" +
+          "<span>" +
+          "Jumlah Laporan : " +
+          value +
+          "</span>" +
+          "</div>"
+        );
+      },
+    },
+    stroke: {
+      show: true,
+      curve: "smooth",
+      width: 3,
+    },
+    xaxis: {
+      labels: {
+        offsetX: 0,
+        offsetY: 0,
+        style: {
+          colors: "#8480ae",
+          fontSize: "12px",
+          fontFamily: "Poppins",
+        },
+      },
+ 
+    },
+    yaxis: {
+      labels: {
+        offsetX: -10,
+        offsetY: 0,
+        style: {
+          colors: "#8480ae",
+          fontSize: "10px",
+          fontFamily: "Poppins",
+        },
+      },
     updateCallback: null,
   },
+  
   location: {
     chartData: {
       chart: {
