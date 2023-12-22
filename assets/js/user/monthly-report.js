1b52cd07692572d0dfb422c5edf863268c09ef41
fahiraaa06 <fahira1678@gmail.com>
2023-12-22 12:17:59 +0700

    perbaikan grafik 2

--- assets/js/user/monthly-report.js
+++ assets/js/user/monthly-report.js
@@ -102,168 +102,6 @@ const fetchDataFromServer = async () => {
   }
 };
 
-const createApexChart = (chartId, chartType, clickCallback, allChartData) => {
-  try {
-    // Mendapatkan opsi chart dari objek allChartData berdasarkan jenis chart
-    const chartOptions = allChartData[chartType] || {};
-
-    // Default options for chart
-    const defaultOptions = {
-      chart: {
-        height: 240,
-        animations: {
-          enabled: true,
-          easing: "easeinout",
-          speed: 1000,
-        },
-      },
-      xaxis: {
-        categories: Array.from(new Array(12), (_, i) => monthToLabel(i)),
-      },
-      animations: {
-        enabled: true,
-        easing: "easeinout",
-        speed: 1000,
-      },
-      dropShadow: {
-        enabled: true,
-        opacity: 0.1,
-        blur: 1,
-        left: -5,
-        top: 18,
-      },
-      zoom: {
-        enabled: false,
-      },
-      toolbar: {
-        show: false,
-      },
-      colors: ["#02172C"],
-      dataLabels: {
-        enabled: false,
-      },
-      fill: {
-        type: "gradient",
-        gradient: {
-          type: "vertical",
-          shadeIntensity: 1,
-          inverseColors: true,
-          opacityFrom: 0.15,
-          opacityTo: 0.02,
-          stops: [40, 100],
-        },
-      },
-      grid: {
-        borderColor: "#dbeaea",
-        strokeDashArray: 4,
-        xaxis: {
-          lines: {
-            show: true,
-          },
-        },
-        yaxis: {
-          lines: {
-            show: false,
-          },
-        },
-        padding: {
-          top: 0,
-          right: 0,
-          bottom: 0,
-          left: 0,
-        },
-      },
-      legend: {
-        position: "bottom",
-        horizontalAlign: "center",
-        offsetY: 4,
-        fontSize: "14px",
-        markers: {
-          width: 9,
-          height: 9,
-          strokeWidth: 0,
-          radius: 20,
-        },
-        itemMargin: {
-          horizontal: 5,
-          vertical: 0,
-        },
-      },
-      subtitle: {
-        text: "Tren Jumlah Pelanggaran Setiap Bulan",
-        align: "left",
-        margin: 0,
-        offsetX: 0,
-        offsetY: 0,
-        floating: false,
-        style: {
-          fontSize: "15px",
-          color: "text-dark",
-          fontWeight: "bold",
-          marginBottom: "1rem",
-          fontFamily: "Poppins",
-        },
-      },
-      stroke: {
-        show: true,
-        curve: "smooth",
-        width: 3,
-      },
-      xaxis: {
-        labels: {
-          offsetX: 0,
-          offsetY: 0,
-          style: {
-            colors: "#8480ae",
-            fontSize: "12px",
-            fontFamily: "Poppins",
-          },
-        },
-      },
-      yaxis: {
-        labels: {
-          offsetX: -10,
-          offsetY: 0,
-          style: {
-            colors: "#8480ae",
-            fontSize: "10px",
-            fontFamily: "Poppins",
-          },
-        },
-      },
-    };
-
-    // Membuat opsi chart
-    const options = {
-      ...defaultOptions,
-      ...chartOptions,
-    };
-
-    const chart = new ApexCharts(document.getElementById(chartId), options);
-    chart.render();
-
-    // Menangani logika klik
-    if (clickCallback && options.xaxis && options.xaxis.categories) {
-      document.getElementById(chartId).addEventListener("click", function () {
-        try {
-          const selectedDataPoints = chart.w.globals.selectedDataPoints;
-
-          if (selectedDataPoints && selectedDataPoints.length > 0) {
-            const clickedIndex = selectedDataPoints[0].dataPointIndex;
-
-            if (clickCallback) {
-              clickCallback(clickedIndex);
-            }
-          }
-        } catch (error) {
-          console.error("Error handling click event:", error);
-        }
-      });
-    }
-  } catch (error) {
-    console.error(`Error creating ApexChart for ${chartId}:`, error);
-  }
-};
 
 // Fungsi untuk mendapatkan jumlah laporan per bulan
 function getReportsCountByMonth(data, month) {
@@ -304,6 +142,69 @@ function getAreaReportsCount(data, area) {
 
   return count;
 }
+
+// Fungsi untuk membuat chart
+const createApexChart = (chartId, chartType, clickCallback, allChartData) => {
+  try {
+    // Mendapatkan opsi chart dari objek allChartData berdasarkan jenis chart
+    const chartOptions = allChartData[chartType] || {};
+
+    // Default options for chart
+    const defaultOptions = {
+      chart: {
+        animations: {
+          enabled: true,
+          easing: "easeinout",
+          speed: 1000,
+        },
+        dropShadow: {
+          enabled: true,
+          opacity: 0.1,
+          blur: 1,
+          left: -5,
+          top: 18,
+        },
+        toolbar: {
+          show: false,
+        },
+      },
+      colors: ["#02172C"],
+      dataLabels: {
+        enabled: false,
+      },
+    };
+
+    // Membuat opsi chart
+    const options = {
+      ...defaultOptions,
+      ...chartOptions,
+    };
+
+    const chart = new ApexCharts(document.getElementById(chartId), options);
+    chart.render();
+
+    // Menangani logika klik
+    if (clickCallback && options.xaxis && options.xaxis.categories) {
+      document.getElementById(chartId).addEventListener("click", function () {
+        try {
+          const selectedDataPoints = chart.w.globals.selectedDataPoints;
+
+          if (selectedDataPoints && selectedDataPoints.length > 0) {
+            const clickedIndex = selectedDataPoints[0].dataPointIndex;
+
+            if (clickCallback) {
+              clickCallback(clickedIndex);
+            }
+          }
+        } catch (error) {
+          console.error("Error handling click event:", error);
+        }
+      });
+    }
+  } catch (error) {
+    console.error(`Error creating ApexChart for ${chartId}:`, error);
+  }
+};
 
 // Fungsi untuk mendapatkan opsi chart jenis
 function getTypeChartOptions(data) {
@@ -519,7 +420,6 @@ function updateSubtypeChart(data, allChartData) {
   }
 }
 
-
 // Objek chart data
 const allChartData = {
   monthly: {
@@ -622,37 +522,6 @@ const allChartData = {
       fontFamily: "Poppins",
     },
   },
-  tooltip: {
-    backgroundColor: "rgb(255,255,255)",
-    bodyFontColor: "#858796",
-    titleMarginBottom: 10,
-    titleFontColor: "#6e707e",
-    titleFontSize: 14,
-    borderColor: "#dddfeb",
-    borderWidth: 1,
-    xPadding: 20,
-    yPadding: 15,
-    displayColors: false,
-    intersect: false,
-    mode: "index",
-    caretPadding: 10,
-    custom: function ({ series, dataPointIndex }) {
-      const month = options.xaxis.categories[dataPointIndex] || "";
-      const value = series[0]?.[dataPointIndex] || 0;
-      return (
-        '<div style="width: 135px; height: 45px;">' +
-        "<span>" +
-        month +
-        "</span>" +
-        "<br>" +
-        "<span>" +
-        "Jumlah Laporan : " +
-        value +
-        "</span>" +
-        "</div>"
-      );
-    },
-  },
   stroke: {
     show: true,
     curve: "smooth",
@@ -681,7 +550,6 @@ const allChartData = {
     },
     updateCallback: null,
   },
-
   location: {
     chartData: {
       chart: {
@@ -711,7 +579,7 @@ const allChartData = {
       },
       plotOptions: {
         bar: {
-          horizontal: false,
+          horizontal: true,
         },
       },
       series: [
@@ -727,101 +595,11 @@ const allChartData = {
     updateCallback: updateAreaChart,
   },
   type: {
-    chartData: {
-      chart: {
-        height: 240,
-        type: "pie",
-        animations: {
-          enabled: true,
-          easing: "easeinout",
-          speed: 1000,
-        },
-        toolbar: {
-          show: false,
-        },
-      },
-      colors: [
-        "rgba(255, 99, 132, 1)",
-        "rgba(255, 159, 64, 1)",
-        "rgba(255, 205, 86, 1)",
-      ],
-      legend: {
-        position: "bottom",
-        horizontalAlign: "center",
-        offsetY: 4,
-        fontSize: "14px",
-        markers: {
-          width: 9,
-          height: 9,
-          strokeWidth: 0,
-          radius: 20,
-        },
-        itemMargin: {
-          horizontal: 5,
-          vertical: 0,
-        },
-      },
-      tooltip: {
-        backgroundColor: "#ffffff",
-        boxShadow: "0px 2px 8px rgba(0, 0, 0, 0.1)",
-        fontSize: "14px",
-        style: {
-          color: "#343a40",
-        },
-      },
-      dataLabels: {
-        enabled: false,
-      },
-    },
+    chartData: getTypeChartOptions(data),  // Menggunakan fungsi getTypeChartOptions untuk mendapatkan opsi chart
     updateCallback: updateTypeChart,
   },
   subtype: {
-    chartData: {
-      chart: {
-        height: 240,
-        type: "doughnut",
-        animations: {
-          enabled: true,
-          easing: "easeinout",
-          speed: 1000,
-        },
-        toolbar: {
-          show: false,
-        },
-      },
-      colors: [
-        "rgba(255, 99, 132, 1)",
-        "rgba(255, 159, 64, 1)",
-        "rgba(255, 205, 86, 1)",
-      ],
-      legend: {
-        position: "bottom",
-        horizontalAlign: "center",
-        offsetY: 4,
-        fontSize: "14px",
-        markers: {
-          width: 9,
-          height: 9,
-          strokeWidth: 0,
-          radius: 20,
-        },
-        itemMargin: {
-          horizontal: 5,
-          vertical: 0,
-        },
-      },
-      tooltip: {
-        backgroundColor: "#ffffff",
-        boxShadow: "0px 2px 8px rgba(0, 0, 0, 0.1)",
-        fontSize: "14px",
-        style: {
-          color: "#343a40",
-        },
-      },
-      dataLabels: {
-        enabled: false,
-      },
-    },
+    chartData: getSubtypeChartOptions(data),  // Menggunakan fungsi getSubtypeChartOptions untuk mendapatkan opsi chart
     updateCallback: updateSubtypeChart,
   },
 };
