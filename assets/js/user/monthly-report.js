5f1ccacd9e12e2ad0a39decfd3c876965338e49a
fahiraaa06 <fahira1678@gmail.com>
2023-12-24 18:45:22 +0700

    perbakan data

--- assets/js/user/monthly-report.js
+++ assets/js/user/monthly-report.js
@@ -434,139 +434,129 @@ const createChartConfig = (chartTitle, data, chartType) => {
           },
         ],
       };
+
     case "locationChart":
       return {
         chart: {
-          height: 380,
-          type: "bar",
-          toolbar: {
-            show: false,
-          },
-        },
-        colors: ["#02172C"],
-        dataLabels: {
-          enabled: false,
-        },
-        fill: {
-          type: "gradient",
-          gradient: {
-            type: "vertical",
-            shadeIntensity: 1,
-            inverseColors: true,
-            opacityFrom: 0.15,
-            opacityTo: 0.02,
-            stops: [40, 100],
-          },
-        },
-        grid: {
-          borderColor: "#dbeaea",
-          strokeDashArray: 4,
-          xaxis: {
-            lines: {
-              show: true,
-            },
-          },
-          yaxis: {
-            lines: {
-              show: false,
-            },
-          },
-          padding: {
-            top: 0,
-            right: 0,
-            bottom: 0,
-            left: 0,
-          },
-        },
-        legend: {
-          position: "bottom",
-          horizontalAlign: "center",
-          offsetY: 4,
-          fontSize: "14px",
-          markers: {
-            width: 9,
-            height: 9,
-            strokeWidth: 0,
-            radius: 20,
-          },
-          itemMargin: {
-            horizontal: 5,
-            vertical: 0,
-          },
-        },
-        tooltip: {
-          theme: "dark",
-          marker: {
-            show: true,
-          },
-          x: {
-            show: false,
-          },
-        },
-        subtitle: {
-          text: subtitleText,
-          align: "left",
-          margin: 0,
-          offsetX: 0,
-          offsetY: 0,
-          floating: false,
-          style: {
-            fontSize: "15px",
-            color: "text-dark",
-            fontWeight: "bold",
-            marginBottom: "10rem",
-            fontFamily: "Poppins",
-          },
-        },
-        plotOptions: {
-          bar: {
-            horizontal: true,
-            barHeight: "80%",
-            distributed: true,
-            dataLabels: {
-              position: "bottom",
-            },
-          },
-        },
-        stroke: {
+      height: 380,
+      type: "bar",
+      toolbar: {
+        show: false,
+      },
+    },
+    colors: ["#02172C"],
+    dataLabels: {
+      enabled: false,
+    },
+    fill: {
+      type: "gradient",
+      gradient: {
+        type: "vertical",
+        shadeIntensity: 1,
+        inverseColors: true,
+        opacityFrom: 0.15,
+        opacityTo: 0.02,
+        stops: [40, 100],
+      },
+    },
+    grid: {
+      borderColor: "#dbeaea",
+      strokeDashArray: 4,
+      xaxis: {
+        lines: {
           show: true,
-          curve: "smooth",
-          width: 3,
         },
-        xaxis: {
-          categories: xCategories,
-          type: 'category',  // Menetapkan tipe data kategori untuk sumbu x
-          labels: {
-            offsetX: 0,
-            offsetY: 0,
-            style: {
-              colors: "#8480ae",
-              fontSize: "12px",
-              fontFamily: "Poppins",
-            },
-          },
-          tooltip: {
-            enabled: false,
-          },
+      },
+      yaxis: {
+        lines: {
+          show: false,
         },
-        yaxis: {
-          labels: {
-            offsetX: -10,
-            offsetY: 0,
-            style: {
-              colors: "#8480ae",
-              fontSize: "12px",
-              fontFamily: "Poppins",
-            },
-          },
+      },
+      padding: {
+        top: 0,
+        right: 0,
+        bottom: 0,
+        left: 0,
+      },
+    },
+    legend: {
+      position: "bottom",
+      horizontalAlign: "center",
+      offsetY: 4,
+      fontSize: "14px",
+      markers: {
+        width: 9,
+        height: 9,
+        strokeWidth: 0,
+        radius: 20,
+      },
+      itemMargin: {
+        horizontal: 5,
+        vertical: 0,
+      },
+    },
+    tooltip: {
+      theme: "dark",
+      marker: {
+        show: true,
+      },
+      x: {
+        show: false,
+      },
+    },
+    subtitle: {
+      text: subtitleText,
+      align: "left",
+      margin: 0,
+      offsetX: 0,
+      offsetY: 0,
+      floating: false,
+      style: {
+        fontSize: "15px",
+        color: "text-dark",
+        fontWeight: "bold",
+        marginBottom: "10rem",
+        fontFamily: "Poppins",
+      },
+    },
+    stroke: {
+      show: true,
+      curve: "smooth",
+      width: 3,
+    },
+    xaxis: {
+      categories: xCategories,
+      labels: {
+        offsetX: 0,
+        offsetY: 0,
+        style: {
+          colors: "#8480ae",
+          fontSize: "12px",
+          fontFamily: "Poppins",
         },
-        series: [
-          {
-            name: "Jumlah Laporan",
-            data: seriesData,
-          },
-        ],
-      };
+      },
+      tooltip: {
+        enabled: false,
+      },
+    },
+    yaxis: {
+      labels: {
+        offsetX: -10,
+        offsetY: 0,
+        style: {
+          colors: "#8480ae",
+          fontSize: "12px",
+          fontFamily: "Poppins",
+        },
+      },
+    },
+    series: [
+      {
+        name: "Jumlah Laporan",
+        data: seriesData,
+      },
+    ],
+  };
     case "areaChart":
       return {
         chart: {
