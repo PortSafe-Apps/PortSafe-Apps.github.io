5dd2fe9695b8d156b79b1725c8c3c17d6e3e5428
fahiraaa06 <fahira1678@gmail.com>
2023-12-27 00:26:25 +0700

    modified user active

--- assets/js/admin/dashboard/user-active.js
+++ assets/js/admin/dashboard/user-active.js
@@ -1,13 +1,15 @@
 // Fungsi untuk menampilkan jumlah total data report dengan progress bar
 const displayUserReports = (data, containerId) => {
+    // Mendapatkan elemen dengan ID containerId
     const userActiveElement = document.getElementById(containerId);
 
+    // Memeriksa apakah elemen ditemukan
     if (!userActiveElement) {
         console.error(`Elemen dengan ID "${containerId}" tidak ditemukan.`);
         return;
     }
 
-    // Hapus semua elemen anak dari userActiveElement
+    // Mengosongkan semua elemen anak dari userActiveElement
     userActiveElement.innerHTML = '';
 
     // Membuat objek untuk menyimpan jumlah laporan setiap user
@@ -31,25 +33,29 @@ const displayUserReports = (data, containerId) => {
     sortedUsers.forEach((userId) => {
         const reportsCount = userReportsCount[userId];
 
+        // Membuat elemen cardBody
         const cardBody = document.createElement('div');
         cardBody.classList.add('card-body');
 
+        // Membuat elemen heading
         const heading = document.createElement('h4');
         heading.classList.add('small', 'font-weight-bold');
         heading.innerText = `User ID: ${userId} - Nama: ${data.find(report => report.user.nipp === userId)?.user.nama || 'Nama Tidak Ditemukan'}`;
 
+        // Membuat elemen progressBarContainer
         const progressBarContainer = document.createElement('div');
         progressBarContainer.classList.add('progress', 'mb-4');
 
+        // Membuat elemen progressBar
         const progressBar = document.createElement('div');
         progressBar.classList.add('progress-bar', 'bg-info');
         progressBar.setAttribute('role', 'progressbar');
         progressBar.setAttribute('style', `width: ${reportsCount}%`);
 
-        // Tambahkan progressBar ke progressBarContainer
+        // Menambahkan progressBar ke progressBarContainer
         progressBarContainer.appendChild(progressBar);
 
-        // Tambahkan cardBody, heading, dan progressBarContainer ke userActiveElement
+        // Menambahkan cardBody, heading, dan progressBarContainer ke userActiveElement
         cardBody.appendChild(heading);
         cardBody.appendChild(progressBarContainer);
         userActiveElement.appendChild(cardBody);
@@ -61,15 +67,16 @@ const getallUserReportWithToken = async () => {
     const token = getTokenFromCookies('Login');
 
     if (!token) {
+        // Tangani kesalahan autentikasi jika tidak ada token
         Swal.fire({
-            icon: 'warning',
-            title: 'Authentication Error',
-            text: 'Kamu Belum Login!',
+          icon: "warning",
+          title: "Authentication Error",
+          text: "Kamu Belum Login!",
         }).then(() => {
-            window.location.href = 'https://portsafe-apps.github.io/';
+          window.location.href = "https://portsafe-apps.github.io/";
         });
-        return;
-    }
+        return [];
+      }
 
     const targetURL = 'https://asia-southeast2-ordinal-stone-389604.cloudfunctions.net/GetAllReport';
 
