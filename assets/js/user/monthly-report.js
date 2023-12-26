96343c947c76ef1e481c1abca4aa9cd877ae32e2
fahiraaa06 <fahira1678@gmail.com>
2023-12-26 21:03:13 +0700

    modified color

--- assets/js/user/monthly-report.js
+++ assets/js/user/monthly-report.js
@@ -509,7 +509,7 @@ const createChartConfig = (chartTitle, data, chartType, selectedTypeName) => {
             horizontal: true,
           },
         },
-        colors: ["#02172C"],
+        colors: colorPalette,colors: ["#02172C"],
         dataLabels: {
           enabled: true,
           textAnchor: "start",
@@ -631,7 +631,7 @@ const createChartConfig = (chartTitle, data, chartType, selectedTypeName) => {
             horizontal: true,
           },
         },
-        colors: ["#02172C"],
+        colors: colorPalette,
         dataLabels: {
           enabled: true,
           textAnchor: "start",
