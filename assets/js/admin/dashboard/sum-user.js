11c85f2ca992250ff52ac0b58f215b1ec24ef026
fahiraaa06 <fahira1678@gmail.com>
2023-12-27 18:38:23 +0700

    perbaikan total user

--- assets/js/admin/dashboard/sum-user.js
+++ assets/js/admin/dashboard/sum-user.js
@@ -17,9 +17,13 @@ const displaySumUsers = (data, containerId) => {
         return;
     }
 
+    // Filter data berdasarkan peran
     const filteredData = data.filter(user => user.role !== 'admin');
+
+    // Hitung total pengguna yang tidak termasuk admin
     const totalUsers = filteredData.length;
 
+    // Tampilkan total pengguna pada elemen HTML
     totalUsersElement.innerText = totalUsers;
 };
 
@@ -29,35 +33,36 @@ const getAllRegisteredUsers = async () => {
     if (!token) {
         // Tangani kesalahan autentikasi jika tidak ada token
         Swal.fire({
-          icon: "warning",
-          title: "Authentication Error",
-          text: "Kamu Belum Login!",
+            icon: 'warning',
+            title: 'Authentication Error',
+            text: 'Kamu Belum Login!',
         }).then(() => {
-          window.location.href = "https://portsafe-apps.github.io/";
+            window.location.href = 'https://portsafe-apps.github.io/';
         });
-        return [];
-      }
+        return;
+    }
 
     const targetURL = 'https://asia-southeast2-ordinal-stone-389604.cloudfunctions.net/getAllUser';
 
     const myHeaders = new Headers();
-    myHeaders.append('Login', token);
+    myHeaders.append("Login", token);
 
     const requestOptions = {
-        method: 'POST',
-        headers: myHeaders,
-        redirect: 'follow',
+      method: "GET",
+      headers: myHeaders,
+      redirect: "follow",
     };
+
 
     try {
         const response = await fetch(targetURL, requestOptions);
         console.log('Server Response:', response.status, response.statusText);
 
-        const data = await response.json();
-        console.log('Data from Server:', data);
-
         if (response.ok) {
-            if (data.status === 200) {
+            const data = await response.json();
+            console.log('Data from Server:', data);
+
+            if (data.status === true) {
                 displaySumUsers(data.data, 'total-users');
             } else {
                 console.error('Server response:', data.message || 'Data tidak dapat ditemukan');
@@ -70,5 +75,5 @@ const getAllRegisteredUsers = async () => {
     }
 };
 
+// Panggil fungsi untuk mendapatkan dan menampilkan total pengguna terdaftar
 getAllRegisteredUsers();
-
