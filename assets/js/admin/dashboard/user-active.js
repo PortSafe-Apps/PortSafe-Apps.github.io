e42a8db5421b35df5c2ca7322f3f4228f177d8cc
fahiraaa06 <fahira1678@gmail.com>
2023-12-27 08:45:29 +0700

    perbaikan user active

--- assets/js/admin/dashboard/user-active.js
+++ assets/js/admin/dashboard/user-active.js
@@ -1,13 +1,13 @@
 // Fungsi untuk mendapatkan token dari cookie
 function getTokenFromCookies(cookieName) {
-    const cookies = document.cookie.split(';');
-    for (const cookie of cookies) {
-        const [name, value] = cookie.trim().split('=');
-        if (name === cookieName) {
-            return value;
-        }
+  const cookies = document.cookie.split(";");
+  for (const cookie of cookies) {
+    const [name, value] = cookie.trim().split("=");
+    if (name === cookieName) {
+      return value;
     }
-    return null;
+  }
+  return null;
 }
 
 // Fungsi untuk menampilkan jumlah total data report dengan progress bar
@@ -32,12 +32,12 @@ const displayUserReports = (data, containerId) => {
 
   // Menghitung jumlah laporan setiap user
   dataArray.forEach((report) => {
-    const userId = report.user.nipp;
+    const nipp = report.user.nipp;
 
-    if (!userReportsCount[userId]) {
-      userReportsCount[userId] = 1;
+    if (!userReportsCount[nipp]) {
+      userReportsCount[nipp] = 1;
     } else {
-      userReportsCount[userId]++;
+      userReportsCount[nipp]++;
     }
   });
 
@@ -47,19 +47,21 @@ const displayUserReports = (data, containerId) => {
   );
 
   // Membuat elemen untuk setiap user dan menambahkannya ke userActiveElement
-  sortedUsers.forEach((userId) => {
-    const reportsCount = userReportsCount[userId];
+  sortedUsers.forEach((nipp) => {
+    const reportsCount = userReportsCount[nipp];
 
     // Membuat elemen cardBody
     const cardBody = document.createElement("div");
     cardBody.classList.add("card-body");
 
+    // Mencari data user berdasarkan nipp
+    const userData = data.find((report) => report.user.nipp === nipp)?.user;
+
     // Membuat elemen heading
     const heading = document.createElement("h4");
     heading.classList.add("small", "font-weight-bold");
-    heading.innerText = `User ID: ${userId} - Nama: ${
-      data.find((report) => report.user.nipp === userId)?.user.nama ||
-      "Nama Tidak Ditemukan"
+    heading.innerText = `NIPP: ${nipp} - Nama: ${
+      userData?.nama || "Nama Tidak Ditemukan"
     }`;
 
     // Membuat elemen progressBarContainer
@@ -116,7 +118,7 @@ const getActiveUser = async () => {
     // Periksa struktur objek result
     console.log(result);
 
-    displayUserReports(result, "userActive");
+    displayUserReports(result.data, "userActive");
   } catch (error) {
     console.error("Error fetching data:", error);
   }
