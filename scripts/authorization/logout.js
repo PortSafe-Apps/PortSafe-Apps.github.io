400733ca5eca78b25ab15dae3e2d7ef977818bb0
fahiraaa06 <fahira1678@gmail.com>
2024-01-02 21:25:52 +0700

    perbaikan function

--- scripts/authorization/logout.js
+++ scripts/authorization/logout.js
@@ -1,8 +1,12 @@
 import { deleteCookie } from 'https://jscroot.github.io/cookie/croot.js';
 
-function logout() {
-  deleteCookie('Login');
-  window.location.href = 'https://portsafe-apps.github.io/';
-}
+document.addEventListener('DOMContentLoaded', function () {
+  // Deklarasi fungsi logout
+  function logout() {
+    deleteCookie('Login');
+    window.location.href = 'https://portsafe-apps.github.io/';
+  }
 
-document.getElementById('logoutButton').addEventListener('click', logout);
+  // Menambahkan event listener pada elemen dengan ID 'logoutButton'
+  document.getElementById('logoutButton').addEventListener('click', logout);
+});
