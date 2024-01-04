8647079a6878e8b877665594fca72199705dbed4
fahiraaa06 <fahira1678@gmail.com>
2024-01-04 11:07:03 +0700

    perbaikan halaman

--- scripts/custom.js
+++ scripts/custom.js
@@ -1206,41 +1206,42 @@ document.addEventListener("DOMContentLoaded", () => {
         }
         */
 
-    const generateNomorPelaporan = () => {
-      const tahunSekarang = new Date().getFullYear();
-      const nomorUrut = Math.floor(Math.random() * 1000); // Generate a random number between 0 and 999
-      const nomorPelaporan = `${tahunSekarang}-K3-${nomorUrut
-        .toString()
-        .padStart(3, "0")}`;
-      return nomorPelaporan;
-    };
+    // const generateNomorPelaporan = () => {
+    //   const tahunSekarang = new Date().getFullYear();
+    //   const nomorUrut = Math.floor(Math.random() * 1000); // Generate a random number between 0 and 999
+    //   const nomorPelaporan = `${tahunSekarang}-K3-${nomorUrut
+    //     .toString()
+    //     .padStart(3, "0")}`;
+    //   return nomorPelaporan;
+    // };
 
-    document.getElementById("nomorPelaporan").textContent =
-      generateNomorPelaporan();
+    // document.getElementById("nomorPelaporan").textContent =
+    //   generateNomorPelaporan();
 
-    // Function untuk generate tanggal saat ini
-    function generateTanggalSaatIni() {
-      const options = { year: "numeric", month: "long", day: "numeric" };
-      return new Date().toLocaleDateString("id-ID", options);
-    }
+    // // Function untuk generate tanggal saat ini
+    // function generateTanggalSaatIni() {
+    //   const options = { year: "numeric", month: "long", day: "numeric" };
+    //   return new Date().toLocaleDateString("id-ID", options);
+    // }
 
-    document.getElementById("tanggalPelaporan").textContent =
-      generateTanggalSaatIni();
+    // document.getElementById("tanggalPelaporan").textContent =
+    //   generateTanggalSaatIni();
 
 
-    // Function untuk generate waktu saat ini dengan zona waktu Indonesia
-    function generateWaktuSaatIni() {
-      const options = {
-        hour: "numeric",
-        minute: "numeric",
-        hour12: true,
-        timeZone: "Asia/Jakarta",
-      };
-      return new Date().toLocaleTimeString("id-ID", options);
-    }
+    // // Function untuk generate waktu saat ini dengan zona waktu Indonesia
+    // function generateWaktuSaatIni() {
+    //   const options = {
+    //     hour: "numeric",
+    //     minute: "numeric",
+    //     hour12: true,
+    //     timeZone: "Asia/Jakarta",
+    //   };
+    //   return new Date().toLocaleTimeString("id-ID", options);
+    // }
 
-    document.getElementById("waktuPelaporan").textContent =
-      generateWaktuSaatIni();
+    // document.getElementById("waktuPelaporan").textContent =
+    //   generateWaktuSaatIni();
+
 
     //Dropdown
     var dropdownElementList = [].slice.call(
