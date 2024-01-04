0d5336c3b78f0a138e4460d0f6278a45b49a8663
fahiraaa06 <fahira1678@gmail.com>
2024-01-04 11:12:09 +0700

    perbaikan icob

--- scripts/custom.js
+++ scripts/custom.js
@@ -23,7 +23,7 @@ document.addEventListener("DOMContentLoaded", () => {
   //Place all your custom Javascript functions and plugin calls below this line
   function init_template() {
     //Caching Global Variables
-    var i, e, el;
+    var i, e, el; 
 
     //Greetig Heading
     var pageTitle = document.querySelectorAll(
@@ -1205,43 +1205,6 @@ document.addEventListener("DOMContentLoaded", () => {
             })
         }
         */
-
-    // const generateNomorPelaporan = () => {
-    //   const tahunSekarang = new Date().getFullYear();
-    //   const nomorUrut = Math.floor(Math.random() * 1000); // Generate a random number between 0 and 999
-    //   const nomorPelaporan = `${tahunSekarang}-K3-${nomorUrut
-    //     .toString()
-    //     .padStart(3, "0")}`;
-    //   return nomorPelaporan;
-    // };
-
-    // document.getElementById("nomorPelaporan").textContent =
-    //   generateNomorPelaporan();
-
-    // // Function untuk generate tanggal saat ini
-    // function generateTanggalSaatIni() {
-    //   const options = { year: "numeric", month: "long", day: "numeric" };
-    //   return new Date().toLocaleDateString("id-ID", options);
-    // }
-
-    // document.getElementById("tanggalPelaporan").textContent =
-    //   generateTanggalSaatIni();
-
-
-    // // Function untuk generate waktu saat ini dengan zona waktu Indonesia
-    // function generateWaktuSaatIni() {
-    //   const options = {
-    //     hour: "numeric",
-    //     minute: "numeric",
-    //     hour12: true,
-    //     timeZone: "Asia/Jakarta",
-    //   };
-    //   return new Date().toLocaleTimeString("id-ID", options);
-    // }
-
-    // document.getElementById("waktuPelaporan").textContent =
-    //   generateWaktuSaatIni();
-
 
     //Dropdown
     var dropdownElementList = [].slice.call(
