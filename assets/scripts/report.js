037b8db2b4eb43e6b563a2e7d59b4bf1749a9a23
fahiraaa06 <fahira1678@gmail.com>
2024-01-06 10:27:42 +0700

    perbaikan list report 2

--- assets/scripts/report.js
+++ assets/scripts/report.js
@@ -40,12 +40,11 @@ const getUserReportsByCategory = async (url, category) => {
         if (response.ok) {
             const data = await response.json();
 
-            if (data.status === 200) {
+            if (Array.isArray(data)) {
                 // Memproses dan menampilkan data laporan dalam tab
-                console.log("Data dari server:", data.data);
-                processReportData(data.data, category);
+                processReportData(data, category);
             } else {
-                console.error("Respon server:", data.message || "Data tidak dapat ditemukan");
+                console.error("Format data tidak valid:", data);
             }
         } else {
             console.error("Kesalahan HTTP:", response.status);
@@ -58,20 +57,10 @@ const getUserReportsByCategory = async (url, category) => {
 // Fungsi untuk memproses dan menampilkan data laporan dalam tab
 const processReportData = (data, category) => {
     // Menyesuaikan struktur HTML sesuai dengan kebutuhan
-    const tabContainerId = "tab-controls";
-    const cardContainerId = "cardContainer";
+    const tabContainerId = "tab-group-1";
 
-    // Memeriksa apakah tab untuk kategori sudah ada
-    const existingTab = document.querySelector(`#${tabContainerId} a[data-category="${category}"]`);
-    if (!existingTab) {
-        // Jika tab belum ada, membuat tab baru dan menampilkan laporan
-        console.log("Membuat tab dan menampilkan laporan");
-        createTabAndDisplayReports(category, tabContainerId, cardContainerId, data);
-    } else {
-        // Jika tab sudah ada, mengambil data baru dan memperbarui tab yang ada
-        console.log("Memperbarui tab yang ada");
-        fetchDataAndCreateCards(data, category);
-    }
+    // Membuat tab baru dan menampilkan laporan
+    createTabAndDisplayReports(data, category, tabContainerId);
 };
 
 // Fungsi untuk membuat kartu laporan
@@ -79,91 +68,74 @@ const createReportCard = (report, category) => {
     const newCard = document.createElement("div");
     newCard.className = "card card-style";
 
-   
-  // Menambahkan badge status untuk kategori "Compromised Action"
-  const statusBadge =
-  category === "Compromised Action"
-    ? `<span class="badge bg-green-dark color-white font-10 mb-1 d-block rounded-s">${report.status}</span>`
-    : "";
+    // Menambahkan badge status untuk kategori "Compromised Action"
+    const statusBadge = category === "Compromised Action" ?
+        `<span class="badge bg-green-dark color-white font-10 mb-1 d-block rounded-s">${report.status}</span>` : "";
 
-// Memastikan bahwa properti yang akan diakses tersedia sebelum mengaksesnya
-const locationName = report.location
-  ? report.location.locationName
-  : "Lokasi Tidak Diketahui";
-const typeName =
-  report.typeDangerousActions &&
-  report.typeDangerousActions.length > 0 &&
-  report.typeDangerousActions[0].subTypes &&
-  report.typeDangerousActions[0].subTypes.length > 0 &&
-  report.typeDangerousActions[0].subTypes[0].typeName
-    ? report.typeDangerousActions[0].subTypes[0].typeName
-    : "Jenis Tidak Diketahui";
-const userName = report.user ? report.user.nama : "Pengguna Tidak Diketahui";
+    // Memastikan bahwa properti yang akan diakses tersedia sebelum mengaksesnya
+    const locationName = report.location ? report.location.locationName : "Unknown Location";
+    const typeName = report.typeDangerousActions && report.typeDangerousActions.length > 0 &&
+        report.typeDangerousActions[0].subTypes && report.typeDangerousActions[0].subTypes.length > 0 &&
+        report.typeDangerousActions[0].subTypes[0].typeName
+        ? report.typeDangerousActions[0].subTypes[0].typeName
+        : "Unknown Type";
+    const userName = report.user ? report.user.nama : "Unknown User";
 
-// Sesuaikan struktur kartu dengan data yang Anda miliki
-newCard.innerHTML = `
-    <div class="content">
-        <div class="d-flex">
-            <div>
-                <h4>${report.reportid}</h4>
-                <p class="color-highlight mt-n1 font-12"><i class="fa fa-map-marker-alt"></i>${locationName}</p>
+    // Sesuaikan struktur kartu dengan data yang Anda miliki
+    newCard.innerHTML = `
+        <div class="content">
+            <div class="d-flex">
+                <div>
+                    <h4>${report.reportid}</h4>
+                    <p class="color-highlight mt-n1 font-12"><i class="fa fa-map-marker-alt"></i>${locationName}</p>
+                </div>
+                <div class="ms-auto align-self-center">
+                    ${statusBadge}
+                </div>
             </div>
-            <div class="ms-auto align-self-center">
-                ${statusBadge}
+            <div class="divider bg-highlight mt-0 mb-2"></div>
+            <p class="mb-0 color-highlight">
+                Jenis Ketidaksesuaian
+            </p>
+            <span class="badge bg-highlight color-white font-10 mb-1 rounded-s">${typeName}</span>
+            <div class="row mb-n2 color-theme">
+                <div class="col-5 font-11">
+                    <p class="color-highlight font-11"><i class="fa fa-user"></i> ${userName}</p>
+                </div>
+                <div class="col-7 font-11">
+                    <p class="color-highlight font-11"><i class="far fa-calendar"></i> ${report.date} <i class="ms-4 far fa-clock"></i> ${report.time}</p>
+                </div>
             </div>
         </div>
-        <div class="divider bg-highlight mt-0 mb-2"></div>
-        <p class="mb-0 color-highlight">
-            Jenis Ketidaksesuaian
-        </p>
-        <span class="badge bg-highlight color-white font-10 mb-1 rounded-s">${typeName}</span>
-        <div class="row mb-n2 color-theme">
-            <div class="col-5 font-11">
-                <p class="color-highlight font-11"><i class="fa fa-user"></i> ${userName}</p>
-            </div>
-            <div class="col-7 font-11">
-                <p class="color-highlight font-11"><i class="far fa-calendar"></i> ${report.date} <i class="ms-4 far fa-clock"></i> ${report.time}</p>
-            </div>
-        </div>
-    </div>
-  `;
-
+    `;
 
     return newCard;
 };
 
 // Fungsi untuk membuat tab dan menampilkan laporan
-const createTabAndDisplayReports = (category, tabContainerId, cardContainerId, data) => {
+const createTabAndDisplayReports = async (data, category, tabContainerId) => {
     const tabContainer = document.getElementById(tabContainerId);
-    const cardContainer = document.getElementById(cardContainerId);
 
     // Membuat tab
     const tabLink = document.createElement("a");
     tabLink.href = "#";
+    tabLink.dataset.active = true;
     tabLink.dataset.bsToggle = "collapse";
-    tabLink.dataset.bsTarget = `#${category}`;
+    tabLink.dataset.bsTarget = `#tab-${category.split(" ").join("-")}`;
     tabLink.innerHTML = category;
     tabContainer.appendChild(tabLink);
 
     // Membuat card container
     const cardCollapse = document.createElement("div");
-    cardCollapse.className = "collapse";
-    cardCollapse.id = category;
-    cardContainer.appendChild(cardCollapse);
+    cardCollapse.className = "collapse show";
+    cardCollapse.id = `tab-${category.split(" ").join("-")}`;
+    tabContainer.appendChild(cardCollapse);
 
     // Menampilkan laporan dalam card container
-    if (Array.isArray(data)) {
-        data.forEach(report => {
-            const newCard = createReportCard(report, category);
-            cardCollapse.appendChild(newCard);
-        });
-    } else if (data && typeof data === 'object') {
-        // Jika data adalah objek, membuat satu card
-        const newCard = createReportCard(data, category);
+    data.forEach(report => {
+        const newCard = createReportCard(report, category);
         cardCollapse.appendChild(newCard);
-    } else {
-        console.error("Format data tidak didukung:", data);
-    }
+    });
 };
 
 // Memanggil fungsi untuk mendapatkan laporan berdasarkan kategori "Unsafe Action"
