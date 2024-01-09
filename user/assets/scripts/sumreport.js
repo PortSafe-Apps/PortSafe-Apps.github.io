937e5643ed3c293a6d7e570f647dd60031f93755
fahiraaa06 <fahira1678@gmail.com>
2024-01-09 22:25:45 +0700

    perbaikan monthly report

--- user/assets/scripts/sumreport.js
+++ user/assets/scripts/sumreport.js
@@ -23,6 +23,32 @@ const displaySumReports = (combinedTotalReports, containerId) => {
     totalReportsElement.innerText = combinedTotalReports;
 };
 
+// Fungsi untuk menampilkan jumlah total data report unsafe
+const displaySumReportsUnsafe = (unsafeTotalReports, containerId) => {
+    const unsafetotalReportsElement = document.getElementById(containerId);
+
+    if (!unsafetotalReportsElement) {
+        console.error(`Elemen dengan ID "${containerId}" tidak ditemukan.`);
+        return;
+    }
+
+    // Tampilkan jumlah total data report
+    unsafetotalReportsElement.innerText = unsafeTotalReports;
+};
+
+// Fungsi untuk menampilkan jumlah total data report compromised
+const displaySumReportsCompromised = (compromisedTotalReports, containerId) => {
+    const compromisedtotalReportsElement = document.getElementById(containerId);
+
+    if (!compromisedtotalReportsElement) {
+        console.error(`Elemen dengan ID "${containerId}" tidak ditemukan.`);
+        return;
+    }
+
+    // Tampilkan jumlah total data report compromised
+    compromisedtotalReportsElement.innerText = compromisedTotalReports;
+};
+
 // Fungsi untuk mengambil data dari API dan jumlah data laporan yang telah dibuat
 const getAllUserReports = async (token, targetURL) => {
     const myHeaders = new Headers();
@@ -49,9 +75,62 @@ const getAllUserReports = async (token, targetURL) => {
         return 0; // Return 0 if there is an error
     }
 };
+// Fungsi untuk mengambil data laporan unsafe dari API
+const getUnsafeReports = async (token, targetURL) => {
+    const myHeaders = new Headers();
+    myHeaders.append('Login', token);
 
-// Fungsi untuk menghitung total laporan dari kedua URL dan menampilkan hasilnya
-const calculateAndDisplayTotalReports = async () => {
+    const requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        redirect: 'follow',
+    };
+
+    try {
+        const response = await fetch(targetURL, requestOptions);
+        const data = await response.json();
+
+        if (data.status === 200) {
+            return data.data.length; // Return the number of unsafe reports for this URL
+        } else {
+            console.error('Server response:', data.message || 'Data tidak dapat ditemukan');
+            return 0; // Return 0 if there is an error
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        return 0; // Return 0 if there is an error
+    }
+};
+
+// Fungsi untuk mengambil data laporan compromised dari API
+const getCompromisedReports = async (token, targetURL) => {
+    const myHeaders = new Headers();
+    myHeaders.append('Login', token);
+
+    const requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        redirect: 'follow',
+    };
+
+    try {
+        const response = await fetch(targetURL, requestOptions);
+        const data = await response.json();
+
+        if (data.status === 200) {
+            return data.data.length; // Return the number of compromised reports for this URL
+        } else {
+            console.error('Server response:', data.message || 'Data tidak dapat ditemukan');
+            return 0; // Return 0 if there is an error
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        return 0; // Return 0 if there is an error
+    }
+};
+
+// Fungsi untuk menghitung total laporan unsafe dan compromised dan menampilkan hasilnya
+const calculateAndDisplayTotalUnsafeCompromisedReports = async () => {
     const token = getTokenFromCookies('Login');
 
     if (!token) {
@@ -65,21 +144,23 @@ const calculateAndDisplayTotalReports = async () => {
         return;
     }
 
-    const url1 = 'https://asia-southeast2-ordinal-stone-389604.cloudfunctions.net/GetAllReportCompromisedbyUser';
-    const url2 = 'https://asia-southeast2-ordinal-stone-389604.cloudfunctions.net/GetAllReportbyUser';
+    const compromised = 'https://asia-southeast2-ordinal-stone-389604.cloudfunctions.net/GetAllReportCompromisedbyUser';
+    const unsafe = 'https://asia-southeast2-ordinal-stone-389604.cloudfunctions.net/GetAllReportbyUser';
 
-    // Call the function for the first URL and get the number of reports
-    const totalReportsUrl1 = await getAllUserReports(token, url1);
+    // Call the function for the first URL and get the number of compromised reports
+    const totalReportsCompromised = await getCompromisedReports(token, compromised);
 
-    // Call the function for the second URL and get the number of reports
-    const totalReportsUrl2 = await getAllUserReports(token, url2);
+    // Call the function for the second URL and get the number of unsafe reports
+    const totalReportsUnsafe = await getUnsafeReports(token, unsafe);
 
     // Calculate the combined total
-    const combinedTotalReports = totalReportsUrl1 + totalReportsUrl2;
+    const combinedTotalReports = totalReportsCompromised + totalReportsUnsafe;
 
     // Display the combined total
     displaySumReports(combinedTotalReports, 'combined-total-reports');
+    displaySumReportsCompromised(totalReportsCompromised, 'compromised-total-reports');
+    displaySumReportsUnsafe(totalReportsUnsafe, 'unsafe-total-reports');
 };
 
-// Panggil fungsi untuk menghitung total laporan dari kedua URL dan menampilkan hasilnya
-calculateAndDisplayTotalReports();
+// Panggil fungsi untuk menghitung total laporan unsafe dan compromised dan menampilkan hasilnya
+calculateAndDisplayTotalUnsafeCompromisedReports();
