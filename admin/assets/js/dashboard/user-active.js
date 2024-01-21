83010f0be24afc9c3bba9fb3f0cfdea566007bd8
fahiraaa06 <fahira1678@gmail.com>
2024-01-22 02:41:11 +0700

    perbaikan

--- admin/assets/js/dashboard/user-active.js
+++ admin/assets/js/dashboard/user-active.js
@@ -10,6 +10,83 @@ function getTokenFromCookies(cookieName) {
   return null;
 }
 
+// Fungsi untuk menampilkan jumlah total data report dengan progress bar
+const displayUserReports = (data, sortedUsers, containerId) => {
+  const userActiveElement = document.getElementById(containerId);
+
+  if (!userActiveElement) {
+    console.error(`Elemen dengan ID "${containerId}" tidak ditemukan.`);
+    return;
+  }
+
+  userActiveElement.innerHTML = "";
+
+  const userReportsCount = {};
+
+  const dataArray = Array.isArray(data) ? data : [data];
+
+  dataArray.forEach((report) => {
+    const nipp = report.user.nipp;
+
+    if (!userReportsCount[nipp]) {
+      userReportsCount[nipp] = 1;
+    } else {
+      userReportsCount[nipp]++;
+    }
+  });
+
+  if (!sortedUsers || !Array.isArray(sortedUsers) || sortedUsers.length === 0) {
+    console.error("Sorted users data is undefined, not an array, or an empty array.");
+    return;
+  }
+
+  sortedUsers.forEach((nipp) => {
+    const reportsCount = userReportsCount[nipp];
+    const userData = data.find((report) => report.user.nipp === nipp)?.user;
+
+    const userInfoContainer = document.createElement("div");
+    userInfoContainer.classList.add(
+      "d-flex",
+      "align-items-center",
+      "justify-content-between",
+      "small",
+      "mb-1"
+    );
+
+    const titleDiv = document.createElement("div");
+    titleDiv.classList.add("fw-bold");
+    titleDiv.innerText = `${nipp} - ${userData?.nama || "Nama Tidak Ditemukan"}`;
+
+    const percentageDiv = document.createElement("div");
+    percentageDiv.classList.add("small");
+    percentageDiv.innerText = `${reportsCount}%`;
+
+    userInfoContainer.appendChild(titleDiv);
+    userInfoContainer.appendChild(percentageDiv);
+
+    const progressBarContainer = document.createElement("div");
+    progressBarContainer.classList.add("progress", "mb-0");
+
+    const progressBar = document.createElement("div");
+    progressBar.classList.add("progress-bar", "bg-dark");
+    progressBar.setAttribute("role", "progressbar");
+    progressBar.setAttribute("style", `width: ${reportsCount}%`);
+    progressBar.setAttribute("aria-valuenow", reportsCount);
+    progressBar.setAttribute("aria-valuemin", "0");
+    progressBar.setAttribute("aria-valuemax", "100");
+
+    progressBarContainer.appendChild(progressBar);
+
+    const cardBody = document.createElement("div");
+    cardBody.classList.add("card-body");
+
+    cardBody.appendChild(userInfoContainer);
+    cardBody.appendChild(progressBarContainer);
+    userActiveElement.appendChild(cardBody);
+  });
+};
+
+
 const getActiveUser = async () => {
   const token = getTokenFromCookies('Login');
 
