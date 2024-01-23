e6bcb62e2d0ede132e0b2baa6c82234caffe7aa6
fahiraaa06 <fahira1678@gmail.com>
2024-01-24 00:32:04 +0700

    perbaikan user

--- admin/assets/js/user.js
+++ admin/assets/js/user.js
@@ -1,132 +1,122 @@
 const showAlert = (message, type, additionalInfo = '', callback) => {
-    console.log(message, type, additionalInfo);
-    if (typeof callback === 'function') {
+  console.log(message, type, additionalInfo);
+  if (typeof callback === 'function') {
       callback();
-    }
-  };
-  
-  const getTokenFromCookies = (cookieName) => {
-    const cookies = document.cookie.split(';');
-    for (const cookie of cookies) {
+  }
+};
+
+const getTokenFromCookies = (cookieName) => {
+  const cookies = document.cookie.split(';');
+  for (const cookie of cookies) {
       const [name, value] = cookie.trim().split('=');
       if (name === cookieName) {
-        return value;
+          return value;
       }
-    }
-    return null;
-  };
-  
-  const getUserWithToken = async () => {
-    const token = getTokenFromCookies('Login');
-  
-    if (!token) {
+  }
+  return null;
+};
+
+const getUserWithToken = async () => {
+  const token = getTokenFromCookies('Login');
+
+  if (!token) {
       showAlert("Token tidak ditemukan", 'error');
       return;
-    }
-  
-    const targetURL = 'https://asia-southeast2-ordinal-stone-389604.cloudfunctions.net/getAllUser';
-  
-    const myHeaders = new Headers();
-    myHeaders.append('Login', token);
-  
-    const requestOptions = {
+  }
+
+  const targetURL = 'https://asia-southeast2-ordinal-stone-389604.cloudfunctions.net/getAllUser';
+
+  const myHeaders = new Headers();
+  myHeaders.append('Login', token);
+
+  const requestOptions = {
       method: 'GET',
       headers: myHeaders,
       redirect: 'follow'
-    };
-  
-    try {
+  };
+
+  try {
       const response = await fetch(targetURL, requestOptions);
       const data = await response.json();
-  
+
       if (data.status === true) {
-        displayUserData(data.data);
+          displayUserData(data.data);
       } else {
-        showAlert(data.message, 'error');
+          showAlert(data.message, 'error');
       }
-    } catch (error) {
+  } catch (error) {
       console.error('Error:', error);
-    }
-  };
-  
-  const deleteUser = async (username) => {
-    const token = getTokenFromCookies('Login');
-  
-    if (!token) {
+  }
+};
+
+const deleteUser = async (nipp) => {
+  const token = getTokenFromCookies('Login');
+
+  if (!token) {
       showAlert('Authentication Error', 'error', 'You are not logged in.');
       return;
-    }
-  
-    const targetURL = 'https://asia-southeast2-gis-project-401902.cloudfunctions.net/delete-user';
-  
-    const myHeaders = new Headers();
-    myHeaders.append('Login', token);
-    myHeaders.append('Content-Type', 'application/json');
-  
-    const requestOptions = {
+  }
+
+  const targetURL = 'https://asia-southeast2-ordinal-stone-389604.cloudfunctions.net/DeleteUser';
+
+  const myHeaders = new Headers();
+  myHeaders.append('Login', token);
+  myHeaders.append('Content-Type', 'application/json');
+
+  const requestOptions = {
       method: 'DELETE',
       headers: myHeaders,
-      body: JSON.stringify({ username: username }),
+      body: JSON.stringify({ nipp: nipp }),
       redirect: 'follow',
-    };
-  
-    try {
+  };
+
+  try {
       const response = await fetch(targetURL, requestOptions);
       const data = await response.json();
-  
+
       if (data.status === true) {
-        showAlert('Success', 'success', 'User deleted successfully!', () => {
-          getUserWithToken();
-        });
+          showAlert('Success', 'success', 'User deleted successfully!', () => {
+              getUserWithToken();
+          });
       } else {
-        showAlert('Error', 'error', data.message);
+          showAlert('Error', 'error', data.message);
       }
-    } catch (error) {
+  } catch (error) {
       console.error('Error:', error);
-    }
-  };
-  
-  const handleDeleteUser = (username) => {
-    deleteUser(username);
-  };
-  
-  const displayUserData = (userData) => {
-    const userDataBody = document.getElementById('userDataBody');
-  
-    if (userData && userData.length > 0) {
+  }
+};
+
+const handleDeleteUser = (nipp) => {
+  deleteUser(nipp);
+};
+
+const displayUserData = (userData) => {
+  const userDataBody = document.getElementById('datatablesSimple').getElementsByTagName('tbody')[0];
+
+  if (userData && userData.length > 0) {
       userData.forEach(user => {
-        const newRow = document.createElement('tr');
-        newRow.innerHTML = `
-          <td class="px-4 py-3">
-            <div class="flex items-center text-sm">
-              <div>
-                <p class="font-semibold">${user.username}</p>
-              </div>
-            </div>
-          </td>
-          <td class="px-4 py-3 text-sm">
-            <p class="font-semibold">${user.password}</p>
-          </td>
-          <td class="px-4 py-3">
-            <a href="#" class="delete-link" data-employeeid="${user.username}">Delete</a>
-          </td>
-        `;
-        userDataBody.appendChild(newRow);
-  
-        const deleteLink = newRow.querySelector('.delete-link');
-        deleteLink.addEventListener('click', (event) => {
-          event.preventDefault();
-          const username = event.target.getAttribute('data-employeeid');
-          confirmDeleteUser(username);
-        });
+          const newRow = document.createElement('tr');
+          newRow.innerHTML = `
+              <td>${user.nipp}</td>
+              <td>${user.nama}</td>
+              <td>${user.jabatan}</td>
+              <td>${user.location ? user.location.unitKerja : ''}</td>
+              <td>${user.role}</td>
+              <td>${user.timestamp ? new Date(user.timestamp).toLocaleDateString() : ''}</td>
+              <td>
+                  <a class="btn btn-datatable btn-icon btn-transparent-dark me-2" href="user-management-edit-user.html"><i data-feather="edit"></i></a>
+                  <a class="btn btn-datatable btn-icon btn-transparent-dark" href="#!" onclick="confirmDeleteUser('${user.nipp}')"><i data-feather="trash-2"></i></a>
+              </td>
+          `;
+          userDataBody.appendChild(newRow);
       });
-    } else {
-      userDataBody.innerHTML = '<tr><td colspan="3">No user data found.</td></tr>';
-    }
-  };
-  
-  const confirmDeleteUser = (username) => {
-    Swal.fire({
+  } else {
+      userDataBody.innerHTML = '<tr><td colspan="7">No user data found.</td></tr>';
+  }
+};
+
+const confirmDeleteUser = (nipp) => {
+  Swal.fire({
       title: 'Are you sure?',
       text: 'You won\'t be able to revert this!',
       icon: 'warning',
@@ -134,11 +124,11 @@ const showAlert = (message, type, additionalInfo = '', callback) => {
       confirmButtonColor: '#3085d6',
       cancelButtonColor: '#d33',
       confirmButtonText: 'Yes, delete it!'
-    }).then((result) => {
+  }).then((result) => {
       if (result.isConfirmed) {
-        handleDeleteUser(username);
+          handleDeleteUser(nipp);
       }
-    });
-  };
-  
-  getUserWithToken();
+  });
+};
+
+getUserWithToken();
