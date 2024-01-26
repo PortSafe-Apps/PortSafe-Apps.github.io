42e1b4a22b11e1c32b205939fbbeda3ae1de0c5e
fahiraaa06 <fahira1678@gmail.com>
2024-01-26 18:44:51 +0700

    perbaikan

--- admin/assets/js/user/alluser.js
+++ admin/assets/js/user/alluser.js
@@ -1,143 +1,168 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const getTokenFromCookies = (cookieName) => {
-    const cookies = document.cookie.split(";").map(cookie => cookie.trim().split("="));
-    const cookie = cookies.find(([name]) => name === cookieName);
-    return cookie ? cookie[1] : null;
-  };
+const getTokenFromCookies = (cookieName) => {
+  const cookies = document.cookie
+    .split(";")
+    .map((cookie) => cookie.trim().split("="));
+  const cookie = cookies.find(([name]) => name === cookieName);
+  return cookie ? cookie[1] : null;
+};
 
-  const showAlert = (icon, title, text, callback) => {
+const getAllUser = async () => {
+  const token = getTokenFromCookies("Login");
+
+  if (!token) {
     Swal.fire({
-      icon: icon,
-      title: title,
-      text: text,
-    }).then(callback);
-  };
-
-  const handleAuthenticationError = () => {
-    showAlert('warning', 'Authentication Error', 'You are not logged in.', () => {
-      window.location.href = 'https://portsafe-apps.github.io/';
+      icon: "warning",
+      title: "Authentication Error",
+      text: "You are not logged in.",
+    }).then(() => {
+      window.location.href = "https://portsafe-apps.github.io/";
     });
+    return;
+  }
+
+  const targetURL =
+    "https://asia-southeast2-ordinal-stone-389604.cloudfunctions.net/getAllUser";
+
+  const myHeaders = new Headers();
+  myHeaders.append("Login", token);
+
+  const requestOptions = {
+    method: "GET",
+    headers: myHeaders,
+    redirect: "follow",
   };
 
-  const getAllUser = async () => {
-    try {
-      const token = getTokenFromCookies('Login');
-      if (!token) {
-        handleAuthenticationError();
-        return;
-      }
+  try {
+    const response = await fetch(targetURL, requestOptions);
+    const data = await response.json();
 
-      const targetURL = 'https://asia-southeast2-ordinal-stone-389604.cloudfunctions.net/getAllUser';
-
-      const myHeaders = new Headers();
-      myHeaders.append('Login', token);
-
-      const requestOptions = {
-        method: 'GET',
-        headers: myHeaders,
-        redirect: 'follow',
-      };
-
-      const response = await fetch(targetURL, requestOptions);
-      const data = await response.json();
-
-      console.log('Response Data:', data); // Add this line for debugging
-
-      if (data.status === true) {
-        displayUserData(data.data, 'datatablesSimple'); // Assuming 'datatablesSimple' is the correct ID
-      } else {
-        showAlert('error', 'Error', `Server Error: ${data.message}`);
-      }
-    } catch (error) {
-      console.error('Error:', error.message);
+    if (data.status === 200) {
+      displayUserData(data.data, "UserDataBody");
+    } else {
+      Swal.fire({
+        icon: "error",
+        title: "Error",
+        text: data.message,
+      });
     }
+  } catch (error) {
+    console.error("Error:", error);
+  }
+};
+
+const deleteUser = async (nipp) => {
+  const token = getTokenFromCookies("Login");
+
+  if (!token) {
+    Swal.fire({
+      icon: "warning",
+      title: "Authentication Error",
+      text: "You are not logged in.",
+    }).then(() => {
+      window.location.href = "https://portsafe-apps.github.io/admin/user-management-list.html";
+    });
+    return;
+  }
+
+  const targetURL =
+    "https://asia-southeast2-ordinal-stone-389604.cloudfunctions.net/deleteUser";
+
+  const myHeaders = new Headers();
+  myHeaders.append("Login", token);
+  myHeaders.append("Content-Type", "application/json");
+
+  const requestOptions = {
+    method: "DELETE",
+    headers: myHeaders,
+    body: JSON.stringify({ nipp: nipp }),
+    redirect: "follow",
   };
 
-  const deleteUserHandler = async (nipp) => {
-    try {
-      const token = getTokenFromCookies('Login');
-      if (!token) {
-        handleAuthenticationError();
-        return;
-      }
+  try {
+    const response = await fetch(targetURL, requestOptions);
+    const data = await response.json();
 
-      const targetURL = 'https://asia-southeast2-ordinal-stone-389604.cloudfunctions.net/deleteUser';
-
-      const myHeaders = new Headers();
-      myHeaders.append('Login', token);
-      myHeaders.append('Content-Type', 'application/json');
-
-      const requestOptions = {
-        method: 'DELETE',
-        headers: myHeaders,
-        body: JSON.stringify({ nipp: nipp }),
-        redirect: 'follow',
-      };
-
-      const response = await fetch(targetURL, requestOptions);
-      const data = await response.json();
-
-      console.log('Response Data:', data); // Add this line for debugging
-
-      if (data.status === true) {
-        showAlert('success', 'Success', 'User deleted successfully!', getAllUser);
-      } else {
-        showAlert('error', 'Error', `Server Error: ${data.message}`);
-      }
-    } catch (error) {
-      console.error('Error:', error.message);
-    }
-  };
-
-  const editUser = (nipp) => {
-    window.location.href = `https://portsafe-apps.github.io/admin/user-management-edit-user.html?nipp=${nipp}`;
-  };
-
-  const displayUserData = (userData, tableId) => {
-    const userDataBody = document.querySelector(`#${tableId} tbody`);
-
-    if (!userDataBody) {
-      console.error(`Element with ID ${tableId} not found.`);
-      return;
-    }
-
-    userDataBody.innerHTML = '';
-
-    if (userData && userData.length > 0) {
-      userData.forEach((user) => {
-        const newRow = document.createElement('tr');
-        newRow.innerHTML = `
-          <td>${user.nipp}</td>
-          <td>${user.nama}</td>
-          <td>${user.jabatan}</td>
-          <td>${user.location.locationName}</td>
-          <td>${user.role}</td>
-          <td>${new Date(user.timestamp).toLocaleDateString()}</td>
-          <td>
-              <a class="btn btn-datatable btn-icon btn-transparent-dark me-2 edit-link" href="#!" data-nipp="${user.nipp}"><i data-feather="edit"></i></a>
-              <a class="btn btn-datatable btn-icon btn-transparent-dark delete-link" href="#!" data-nipp="${user.nipp}" data-action="deleteUser"><i data-feather="trash-2"></i></a>
-          </td>
-        `;
-        userDataBody.appendChild(newRow);
+    if (data.status === 200) {
+      Swal.fire({
+        icon: "success",
+        title: "Success",
+        text: "User deleted successfully!",
+      }).then(() => {
+        getAllUser();
       });
     } else {
-      userDataBody.innerHTML = '<tr><td colspan="7">No user data found.</td></tr>';
+      Swal.fire({
+        icon: "error",
+        title: "Error",
+        text: data.message,
+      });
     }
+  } catch (error) {
+    console.error("Error:", error);
+  }
+};
 
-    new simpleDatatables.DataTable(`#${tableId}`);
-    feather.replace();
-  };
+const editUser = (nipp) => {
+  window.location.href = `https://portsafe-apps.github.io/admin/user-management-edit-user.html?nipp=${nipp}`;
+};
 
-  document.getElementById('datatablesSimple').addEventListener('click', (event) => {
-    const target = event.target;
-    if (target.classList.contains('edit-link')) {
-      const nipp = target.getAttribute('data-nipp');
-      editUser(nipp);
-    } else if (target.classList.contains('delete-link')) {
-      const nipp = target.getAttribute('data-nipp');
-      deleteUserHandler(nipp);
+document.getElementById("UserDataBody").addEventListener("click", (event) => {
+  const target = event.target;
+  if (target.classList.contains("edit-link")) {
+    const nipp = target.getAttribute("data-nipp");
+    editUser(nipp);
+  } else if (target.classList.contains("delete-link")) {
+    const nipp = target.getAttribute("data-nipp");
+    deleteUserHandler(nipp);
+  }
+});
+const deleteUserHandler = (nipp) => {
+  Swal.fire({
+    title: "Are you sure?",
+    text: "You won't be able to revert this!",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Yes, delete it!",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      deleteUser(nipp);
     }
   });
+};
 
-  getAllUser();
-});
+const displayUserData = (userData, tableBodyId) => {
+  const userDataBody = document.getElementById(tableBodyId);
+
+  userDataBody.innerHTML = "";
+
+  if (userData && userData.length > 0) {
+    userData.forEach((user) => {
+      const newRow = document.createElement("tr");
+      newRow.innerHTML = `
+      <td>${user.nipp}</td>
+      <td>${user.nama}</td>
+      <td>${user.jabatan}</td>
+      <td>${user.location.locationName}</td>
+      <td>${user.role}</td>
+      <td>${new Date(user.timestamp).toLocaleDateString()}</td>
+      <td>
+          <a class="btn btn-datatable btn-icon btn-transparent-dark me-2 edit-link" href="#!" data-nipp="${
+            user.nipp
+          }"><i data-feather="edit"></i></a>
+          <a class="btn btn-datatable btn-icon btn-transparent-dark delete-link" href="#!" data-nipp="${
+            user.nipp
+          }" data-action="deleteUser"><i data-feather="trash-2"></i></a>
+      </td>
+    `;
+
+      userDataBody.appendChild(newRow);
+    });
+  } else {
+    userDataBody.innerHTML = `<tr><td colspan="9">No user data found.</td></tr>`;
+  }
+  feather.replace();
+};
+
+getAllUser();
