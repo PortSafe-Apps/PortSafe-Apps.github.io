5b9b4cc21f24d4a73272f9352a1e8408310b0633
fahiraaa06 <fahira1678@gmail.com>
2024-01-27 18:20:28 +0700

    perbaikan lagi

--- admin/assets/js/unsafe/allreport.js
+++ admin/assets/js/unsafe/allreport.js
@@ -20,50 +20,51 @@ const getTokenFromCookies = (cookieName) => {
 };
 
 const getUnsafeReports = async () => {
-  try {
-    const token = getTokenFromCookies("Login");
-
-    if (!token) {
-      showAlert("Kamu Belum Login!", "warning");
-      window.location.href = "https://portsafe-apps.github.io/";
-      return;
+    try {
+      const token = getTokenFromCookies("Login");
+  
+      if (!token) {
+        showAlert('Kamu Belum Login!', 'warning');
+        window.location.href = 'https://portsafe-apps.github.io/';
+        return;
+      }
+  
+      const targetURL = "https://asia-southeast2-ordinal-stone-389604.cloudfunctions.net/GetAllReport";
+  
+      const myHeaders = new Headers();
+      myHeaders.append("Login", token);
+      myHeaders.append("Content-Type", "application/json");
+  
+      const requestOptions = {
+        method: "POST",
+        headers: myHeaders,
+        redirect: "follow",
+      };
+  
+      const response = await fetch(targetURL, requestOptions);
+      const data = await response.json();
+      console.log("Received Data:", data); // Log the received data
+  
+      if (data.status === 200) {
+        displayReportData(data.data, reportDataBody);
+      } else {
+        showAlert(data.message, "error");
+      }
+    } catch (error) {
+      console.error("Error:", error.message);
     }
-
-    const targetURL =
-      "https://asia-southeast2-ordinal-stone-389604.cloudfunctions.net/GetAllReport";
-
-    const myHeaders = new Headers();
-    myHeaders.append("Login", token);
-    myHeaders.append("Content-Type", "application/json");
-
-    const requestOptions = {
-      method: "POST",
-      headers: myHeaders,
-      redirect: "follow",
-    };
-
-    const response = await fetch(targetURL, requestOptions);
-    const data = await response.json();
-
-    if (data.status === true) {
-      displayReportData(data.data, reportDataBody);
-    } else {
-      showAlert(data.message, "error");
-    }
-  } catch (error) {
-    console.error("Error:", error.message);
-  }
-};
-
-const displayReportData = (data, reportDataBody) => {
-  try {
-    if (reportDataBody) {
-      reportDataBody.innerHTML = "";
-
-      if (data && data.length > 0) {
-        data.forEach((report) => {
-          const newRow = document.createElement("tr");
-          newRow.innerHTML = `
+  };
+  
+  const displayReportData = (data, reportDataBody) => {
+    try {
+      console.log("displayReportData - data:", data); // Log the data
+      if (reportDataBody) {
+        reportDataBody.innerHTML = "";
+  
+        if (data && data.length > 0) {
+          data.forEach((report) => {
+            const newRow = document.createElement("tr");
+            newRow.innerHTML = `
               <td>${report.reportid}</td>
               <td>${report.date}</td>
               <td>${report.time}</td>
@@ -74,20 +75,20 @@ const displayReportData = (data, reportDataBody) => {
                   <button class="btn btn-datatable btn-icon btn-transparent-dark delete-link" data-reportid="${report.reportid}" data-action="deleteReport"><i data-feather="trash-2"></i> Delete</button>
               </td>
             `;
-          reportDataBody.appendChild(newRow);
-        });
-      } else {
-        const emptyRow = document.createElement("tr");
-        emptyRow.innerHTML = '<td colspan="6">No report data found.</td>';
-        reportDataBody.appendChild(emptyRow);
+            reportDataBody.appendChild(newRow);
+          });
+        } else {
+          const emptyRow = document.createElement("tr");
+          emptyRow.innerHTML = '<td colspan="6">No report data found.</td>';
+          reportDataBody.appendChild(emptyRow);
+        }
+  
+        feather.replace();
       }
-
-      feather.replace();
+    } catch (error) {
+      console.error("Error:", error.message);
     }
-  } catch (error) {
-    console.error("Error:", error.message);
-  }
-};
+  };
 
 const deleteReport = async (reportid) => {
   try {
