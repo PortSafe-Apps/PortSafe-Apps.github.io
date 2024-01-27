4aa1c53e32d5f0c411bbb757de2633efcb4a6f95
fahiraaa06 <fahira1678@gmail.com>
2024-01-27 21:25:18 +0700

    modified update

--- admin/assets/js/compromised/update.js
+++ admin/assets/js/compromised/update.js
@@ -1,144 +1,174 @@
 const getTokenFromCookies = (cookieName) => {
-    const cookies = document.cookie.split(';');
-    for (const cookie of cookies) {
+  const cookies = document.cookie.split(';');
+  for (const cookie of cookies) {
       const [name, value] = cookie.trim().split('=');
       if (name === cookieName) {
-        return value;
+          return value;
       }
-    }
-    return null;
-  };
-  
-  const showAlert = (message) => {
-    Swal.fire({
-      icon: 'success',
+  }
+  return null;
+};
+
+const showAlert = (message, type = 'success') => {
+  Swal.fire({
+      icon: type,
       text: message,
       showConfirmButton: false,
       timer: 1500
-    }).then(() => {
+  }).then(() => {
       window.location.href = 'https://portsafe-apps.github.io/user/compromisedreport.html';
-    });
-  };
-  
-  
-  const searchEmployeeById = async (employeeId) => {
-    const token = getTokenFromCookies('Login');
-  
-    if (!token) {
+  });
+};
+
+const searchCompromisedById = async (reportid) => {
+  const token = getTokenFromCookies('Login');
+
+  if (!token) {
       showAlert("Anda Belum Login", 'error');
       return;
-    }
-  
-    const targetURL = 'https://asia-southeast2-ordinal-stone-389604.cloudfunctions.net/getOneReportCompromised';
-  
-    const myHeaders = new Headers();
-    myHeaders.append('Login', token);
-  
-    const requestOptions = {
+  }
+
+  const targetURL = 'https://asia-southeast2-ordinal-stone-389604.cloudfunctions.net/getOneReportCompromised';
+
+  const myHeaders = new Headers();
+  myHeaders.append('Login', token);
+
+  const requestOptions = {
       method: 'POST',
       headers: myHeaders,
-      body: JSON.stringify({ reportid: reportId }),
+      body: JSON.stringify({ reportid: reportid }),
       redirect: 'follow',
-    };
-    try {
+  };
+  try {
       const response = await fetch(targetURL, requestOptions);
       const data = await response.json();
-  
+
       if (data.status === 200) {
-        populateForm(data.data);
+          populateForm(data.data);
       } else {
-        showAlert(data.message, 'error');
+          showAlert(data.message, 'error');
       }
-    } catch (error) {
+  } catch (error) {
       console.error('Error:', error);
-    }
+  }
+};
+
+const populateForm = (data) => {
+  const setValue = (id, value) => {
+      document.getElementById(id).innerText = value;
   };
-  
-  const populateForm = (reportData) => {
-    const setValue = (id, value) => {
-      document.getElementById(id).value = value;
-    };
-  
-    setValue('employeeIdInput', reportData.employeeid);
-    setValue('nameInput', reportData.name);
-    setValue('emailInput', reportData.email);
-    setValue('phoneInput', reportData.phone);
-    setValue('divisionInput', reportData.division.divName);
-    setValue('usernameInput', reportData.account.username);
-    setValue('passwordInput', reportData.account.password);
-    setValue('basicSalaryInput', reportData.salary['basic-salary']);
-    setValue('honorDivisionInput', reportData.salary['honor-division']);
-  
-    document.getElementById('employeeForm').style.display = 'block';
-  };
-  
-  const updateEmployee = async (event) => {
-    event.preventDefault();
-  
-    const employeeId = document.getElementById('employeeIdInput').value;
-    const token = getTokenFromCookies('Login');
-  
-    if (!token) {
+
+  setValue('noPelaporan', data.reportid);
+  setValue('tanggal', data.date);
+  setValue('waktu', data.time);
+  setValue('pengawas', `${data.user.nama} - ${data.user.jabatan}`);
+  setValue('jabatan', data.user.jabatan);
+  setValue('unitKerja', data.location.locationName);
+  setValue('area', data.area.areaName);
+
+  const descriptionElement = document.getElementById('deskripsiPengamatan');
+  descriptionElement.innerText = data.description;
+
+  const imageElement = document.getElementById('observasiPhoto');
+  imageElement.src = data.observationPhoto;
+
+  const accordionBody = document.getElementById('accordionBody');
+  accordionBody.innerHTML = '';
+  data.typeDangerousActions.forEach((type) => {
+      const subType = type.subTypes[0];
+      accordionBody.innerHTML += `<div class="accordion-item">
+                                      <h2 class="accordion-header" id="flush-heading${type.typeId}">
+                                          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse${type.typeId}" aria-expanded="false" aria-controls="flush-collapse${type.typeId}">
+                                              ${type.typeName}
+                                          </button>
+                                      </h2>
+                                      <div id="flush-collapse${type.typeId}" class="accordion-collapse collapse" aria-labelledby="flush-heading${type.typeId}" data-bs-parent="#accordionFlushExample">
+                                          <div class="accordion-body"><span class="badge bg-danger">${subType}</span></div>
+                                      </div>
+                                  </div>`;
+  });
+
+  setValue('tindakanPerbaikanSegera', data.immediateAction);
+
+  const improvementPhotoElement = document.getElementById('improvementPhoto');
+  improvementPhotoElement.src = data.improvementPhoto;
+
+  const recomendationElement = document.getElementById('rekomendasi');
+  recomendationElement.innerText = data.recomendation;
+
+  const tindakLanjutTextarea = document.getElementById('exampleFormControlTextarea1');
+  tindakLanjutTextarea.value = data.ActionDesc || '';
+};
+
+const updateCompromised = async (event, reportid) => {
+  event.preventDefault();
+
+  const token = getTokenFromCookies('Login');
+
+  if (!token) {
       showAlert("Anda Belum Login", 'error');
       return;
-    }
-  
-    const targetURL = 'https://asia-southeast2-gis-project-401902.cloudfunctions.net/updatedataemployee';
-  
-    const myHeaders = new Headers();
-    myHeaders.append('Login', token);
-    myHeaders.append('Content-Type', 'application/json');
-  
-    const basicSalaryInput = document.getElementById('basicSalaryInput').value;
-    const honorDivisionInput = document.getElementById('honorDivisionInput').value;
-  
-    if (isNaN(basicSalaryInput) || isNaN(honorDivisionInput)) {
-      showAlert('Please enter valid numeric values for salary.', 'error');
-      return;
-    }
-  
-    const requestOptions = {
+  }
+
+  const targetURL = 'https://asia-southeast2-gis-project-401902.cloudfunctions.net/updatedatacompromised';
+
+  const myHeaders = new Headers();
+  myHeaders.append('Login', token);
+  myHeaders.append('Content-Type', 'application/json');
+
+  const requestOptions = {
       method: 'PUT',
       headers: myHeaders,
       body: JSON.stringify({
-        employeeid: employeeId,
-        name: document.getElementById('nameInput').value,
-        email: document.getElementById('emailInput').value,
-        phone: document.getElementById('phoneInput').value,
-        division: { divName: document.getElementById('divisionInput').value },
-        account: {
-          username: document.getElementById('usernameInput').value,
-          password: document.getElementById('passwordInput').value,
-        },
-        salary: {
-          'basic-salary': parseInt(basicSalaryInput),
-          'honor-division': parseInt(honorDivisionInput),
-        },
+          reportid: reportid,
+          date: document.getElementById('tanggal').innerText,
+          time: document.getElementById('waktu').innerText,
+          user: {
+              nipp: document.getElementById('nipp').innerText,
+              nama: document.getElementById('pengawas').innerText,
+              jabatan: document.getElementById('jabatan').innerText,
+              location: {
+                  locationName: document.getElementById('unitKerja').innerText,
+              },
+          },
+          location: {
+              locationName: document.getElementById('unitKerja').innerText,
+          },
+          area: {
+              areaName: document.getElementById('area').innerText,
+          },
+          description: document.getElementById('deskripsiPengamatan').innerText,
+          observationPhoto: document.getElementById('observasiPhoto').src,
+          typeDangerousActions: [], 
+          immediateAction: document.getElementById('tindakanPerbaikanSegera').innerText,
+          improvementPhoto: document.getElementById('improvementPhoto').src,
+          recomendation: document.getElementById('rekomendasiText').innerText,
+          ActionDesc: document.getElementById('exampleFormControlTextarea1').value,
       }),
       redirect: 'follow',
-    };
-  
-    try {
+  };
+
+  try {
       const response = await fetch(targetURL, requestOptions);
       const data = await response.json();
-  
-      if (data.status === 200) {
-        showAlert('Berhasil Update Data', 'success');
-        window.location.href = 'tables_emp.html';
+
+      if (data.status === true) {
+          showAlert('Berhasil Update Data Report', 'success');
+          window.location.href = 'https://portsafe-apps.github.io/admin/compromisedreport.html';
       } else {
-        showAlert(data.message, 'error');
+          showAlert(data.message, 'error');
       }
-    } catch (error) {
+  } catch (error) {
       console.error('Error:', error);
-    }
-  };
-  
-  const employeeIdFromURL = new URLSearchParams(window.location.search).get('employeeid');
-  if (employeeIdFromURL) {
-    document.getElementById('employeeIdInput').value = employeeIdFromURL;
-    searchEmployeeById(employeeIdFromURL);
   }
-  
-  document.getElementById('employeeForm').style.display = 'block';
-  
-  document.getElementById('employeeForm').addEventListener('submit', updateEmployee);
+};
+
+document.getElementById('compromisedForm').style.display = 'block';
+
+const reportIdFromURL = new URLSearchParams(window.location.search).get('reportid');
+if (reportIdFromURL) {
+  document.getElementById('reportIdInput').value = reportIdFromURL;
+  searchCompromisedById(reportIdFromURL);
+}
+
+document.getElementById('updateButton').addEventListener('click', (event) => updateCompromised(event, compromisedIdFromURL));
