423af1041be2fd42144e8a313bc8c6ec177781af
fahiraaa06 <fahira1678@gmail.com>
2024-01-29 01:26:46 +0700

    fixing bug add user

--- admin/assets/js/user/adduser.js
+++ admin/assets/js/user/adduser.js
@@ -1,8 +1,8 @@
 const showAlert = (message, type) => {
   Swal.fire({
-      icon: type,
-      title: 'Success',
-      text: message,
+    icon: type,
+    title: "Success",
+    text: message,
   });
 };
 
@@ -10,61 +10,69 @@ document.addEventListener("DOMContentLoaded", function () {
   const form = document.querySelector("form");
 
   // Define a default endpoint
-  let targetURL = 'https://asia-southeast2-ordinal-stone-389604.cloudfunctions.net/register';
+  let targetURL =
+    "https://asia-southeast2-ordinal-stone-389604.cloudfunctions.net/register";
 
   // You can add an input field or configuration logic to set the endpoint dynamically if needed
 
   form.addEventListener("submit", async function (event) {
-      event.preventDefault();
+    event.preventDefault();
 
-      // Get form input values inside the submit event listener
-      const nipp = document.getElementById('nipp').value;
-      const nama = document.getElementById('nama').value;
-      const selectedJabatan = document.querySelector('input[name="radioJabatan"]:checked');
-      const unitKerja = document.querySelector("#location").value
-      const jabatan = selectedJabatan ? selectedJabatan.value : null;
-      const role = document.querySelector("#role").value
-      const password = document.getElementById('password').value;
+    // Get form input values inside the submit event listener
+    const nipp = document.getElementById("nipp").value;
+    const nama = document.getElementById("nama").value;
+    const selectedJabatan = document.querySelector(
+      'input[name="radioJabatan"]:checked'
+    );
+    const unitKerja = document.querySelector("#location").value;
+    const jabatan = selectedJabatan ? selectedJabatan.value : null;
+    const role = document.querySelector("#role").value;
+    const password = document.getElementById("password").value;
 
-      // Check for empty values
-      if (!nipp || !nama || !jabatan || !unitKerja || !role || !password) {
-          showAlert('Please fill in all fields', 'error');
-          return;
+    // Check for empty values
+    if (!nipp || !nama || !jabatan || !unitKerja || !role || !password) {
+      showAlert("Please fill in all fields", "error");
+      return;
+    }
+
+    // Prepare the request body
+    const requestBody = {
+      nipp,
+      nama,
+      jabatan,
+      Location: {
+        LocationName: unitKerja,
+      },
+      password,
+      role,
+    };
+
+    // Define your headers
+    var myHeaders = new Headers();
+    myHeaders.append("Authorization", "Bearer " + token);
+    myHeaders.append("Content-Type", "application/json");
+
+    const requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: JSON.stringify(requestBody),
+      redirect: "follow",
+    };
+
+    try {
+      // Make the API request
+      const response = await fetch(targetURL, requestOptions);
+      const data = await response.json();
+
+      // Handle the response
+      if (data.status === false) {
+        showAlert(data.message, "error");
+      } else {
+        showAlert("User added successfully!", "success");
+        window.location.href = "user-management-list-user.html";
       }
-
-      // Prepare the request body
-      const requestBody = {
-          nipp,
-          nama,
-          jabatan,
-          Location: {
-              LocationName: unitKerja,
-          },
-          password,
-          role,
-      };
-
-      const requestOptions = {
-          method: 'POST',
-          headers: myHeaders,
-          body: JSON.stringify(requestBody),
-          redirect: 'follow',
-      };
-
-      try {
-          // Make the API request
-          const response = await fetch(targetURL, requestOptions);
-          const data = await response.json();
-
-          // Handle the response
-          if (data.status === false) {
-              showAlert(data.message, 'error');
-          } else {
-              showAlert("User added successfully!", 'success');
-              window.location.href = 'user-management-list-user.html';
-          }
-      } catch (error) {
-          console.error('Error:', error);
-      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
   });
 });
